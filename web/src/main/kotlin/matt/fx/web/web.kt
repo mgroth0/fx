0ecb6f0900bea473f2d004fdf1c417b4a8d8ea07
mgroth0 <mgroth49@gmail.com>
2022-05-30 05:27:58 -0400

    autocommit

--- web/src/main/kotlin/matt/fx/web/web.kt
+++ web/src/main/kotlin/matt/fx/web/web.kt
@@ -2,8 +2,10 @@ package matt.fx.web
 
 import javafx.beans.property.SimpleDoubleProperty
 import javafx.event.EventTarget
+import javafx.scene.layout.Region
 import javafx.scene.web.HTMLEditor
 import javafx.scene.web.WebView
+import javafx.stage.Stage
 import matt.hurricanefx.eye.lang.DProp
 import matt.hurricanefx.tornadofx.fx.attachTo
 import matt.klib.lang.NEVER
@@ -32,4 +34,34 @@ fun EventTarget.webview(op: WebView.() -> Unit = {}) = WebView().attachTo(this, 
 
 fun EventTarget.htmleditor(html: String? = null, op: HTMLEditor.() -> Unit = {}) = HTMLEditor().attachTo(this, op) {
   if (html != null) it.htmlText = html
+}
+
+infix fun WebView.perfectBind(other: Region) {
+  this minBind other
+  this maxBind other
+}
+
+infix fun WebView.perfectBind(other: Stage) {
+  this minBind other
+  this maxBind other
+}
+
+infix fun WebView.maxBind(other: Region) {
+  maxHeightProperty().bind(other.heightProperty()) // gotta be strict with webview, which I think tries to be big
+  maxWidthProperty().bind(other.widthProperty())
+}
+
+infix fun WebView.maxBind(other: Stage) {
+  maxHeightProperty().bind(other.heightProperty()) // gotta be strict with webview, which I think tries to be big
+  maxWidthProperty().bind(other.widthProperty())
+}
+
+infix fun WebView.minBind(other: Region) {
+  minHeightProperty().bind(other.heightProperty())
+  minWidthProperty().bind(other.widthProperty())
+}
+
+infix fun WebView.minBind(other: Stage) {
+  minHeightProperty().bind(other.heightProperty())
+  minWidthProperty().bind(other.widthProperty())
 }
