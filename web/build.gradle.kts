12cf2934accb81f3cc1f89a6a64aeda610ca75f2
mgroth0 <mgroth49@gmail.com>
2022-06-05 13:32:30 -0400

    autocommit

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -3,6 +3,7 @@ modtype = LIB
 dependencies {
   implementation(projects.kj.kjlib.lang)
   implementation(libs.fx.web)
+  implementation(projects.kj.fx.graphics)
   implementation(projects.kj.hurricanefx.eye)
   implementation(projects.kj.hurricanefx)
   implementation(libs.jsoup)
