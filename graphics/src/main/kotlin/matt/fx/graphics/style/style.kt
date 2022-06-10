526171f2b4c58032d6a191ae00f0411979a6eae7
mgroth0 <mgroth49@gmail.com>
2022-06-10 03:00:55 -0400

    autocommit

--- graphics/src/main/kotlin/matt/fx/graphics/style/style.kt
+++ graphics/src/main/kotlin/matt/fx/graphics/style/style.kt
@@ -3,6 +3,7 @@ package matt.fx.graphics.style
 import com.jthemedetecor.OsThemeDetector
 import javafx.application.Platform.runLater
 import javafx.css.Styleable
+import javafx.geometry.Insets
 import javafx.scene.layout.Border
 import javafx.scene.layout.BorderStroke
 import javafx.scene.layout.BorderStrokeStyle
@@ -156,3 +157,59 @@ fun Region.red() {
 fun Region.orange() {
   borderDashFill = Color.ORANGE
 }
+
+
+
+
+
+
+/*part of this file was taken from tornadofx*/
+
+
+
+fun insets(all: Number) = Insets(all.toDouble(), all.toDouble(), all.toDouble(), all.toDouble())
+fun insets(horizontal: Number? = null, vertical: Number? = null) = Insets(
+  vertical?.toDouble() ?: 0.0,
+  horizontal?.toDouble() ?: 0.0,
+  vertical?.toDouble() ?: 0.0,
+  horizontal?.toDouble() ?: 0.0
+)
+
+fun insets(
+  top: Number? = null,
+  right: Number? = null,
+  bottom: Number? = null,
+  left: Number? = null
+) = Insets(
+  top?.toDouble() ?: 0.0,
+  right?.toDouble() ?: 0.0,
+  bottom?.toDouble() ?: 0.0,
+  left?.toDouble() ?: 0.0
+)
+
+fun Insets.copy(
+  top: Number? = null,
+  right: Number? = null,
+  bottom: Number? = null,
+  left: Number? = null
+) = Insets(
+  top?.toDouble() ?: this.top,
+  right?.toDouble() ?: this.right,
+  bottom?.toDouble() ?: this.bottom,
+  left?.toDouble() ?: this.left
+)
+
+
+fun Insets.copy(
+  horizontal: Number? = null,
+  vertical: Number? = null
+) = Insets(
+  vertical?.toDouble() ?: this.top,
+  horizontal?.toDouble() ?: this.right,
+  vertical?.toDouble() ?: this.bottom,
+  horizontal?.toDouble() ?: this.left
+)
+
+val Insets.horizontal get() = (left + right)/2
+val Insets.vertical get() = (top + bottom)/2
+val Insets.all get() = (left + right + top + bottom)/4
