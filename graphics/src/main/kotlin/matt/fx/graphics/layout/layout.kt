5fd104264f75d58e249a8a06f42502041aa6289e
mgroth0 <mgroth49@gmail.com>
2022-06-10 03:07:34 -0400

    autocommit

--- graphics/src/main/kotlin/matt/fx/graphics/layout/layout.kt
+++ graphics/src/main/kotlin/matt/fx/graphics/layout/layout.kt
@@ -37,6 +37,7 @@ import javafx.scene.layout.StackPane
 import javafx.scene.layout.TilePane
 import javafx.scene.layout.VBox
 import javafx.stage.Stage
+import matt.fx.graphics.style.copy
 import matt.hurricanefx.eye.lib.proxypropDouble
 import matt.hurricanefx.tornadofx.fx.getChildList
 import matt.hurricanefx.tornadofx.fx.opcr
@@ -114,16 +115,12 @@ fun VBox.spacer(prio: Priority = Priority.ALWAYS, op: Pane.()->Unit = {}) =
   opcr(this, Pane().apply { VBox.setVgrow(this, prio) }, op)
 
 
-
-
-
-
 private val GridPaneRowIdKey = "TornadoFX.GridPaneRowId"
 private val GridPaneParentObjectKey = "TornadoFX.GridPaneParentObject"
 
-fun GridPane.row(title: String? = null, op: Pane.() -> Unit = {}) {
+fun GridPane.row(title: String? = null, op: Pane.()->Unit = {}) {
   properties[GridPaneRowIdKey] =
-    if (properties.containsKey(GridPaneRowIdKey)) properties[GridPaneRowIdKey] as Int + 1 else 0
+	if (properties.containsKey(GridPaneRowIdKey)) properties[GridPaneRowIdKey] as Int + 1 else 0
 
   // Allow the caller to add children to a fake pane
   val fake = Pane()
@@ -146,23 +143,23 @@ fun GridPane.row(title: String? = null, op: Pane.() -> Unit = {}) {
 fun GridPane.removeRow(node: Node): Int {
   val rowIdKey = properties[GridPaneRowIdKey] as Int?
   if (rowIdKey != null) {
-    when (rowIdKey) {
-      0 -> properties.remove(GridPaneRowIdKey)
-      else -> properties[GridPaneRowIdKey] = rowIdKey - 1
-    }
+	when (rowIdKey) {
+	  0    -> properties.remove(GridPaneRowIdKey)
+	  else -> properties[GridPaneRowIdKey] = rowIdKey - 1
+	}
   }
   val rowIndex = GridPane.getRowIndex(node) ?: 0
   val nodesToDelete = mutableListOf<Node>()
   children.forEach { child ->
-    val childRowIndex = GridPane.getRowIndex(child) ?: 0
-    if (childRowIndex == rowIndex) {
-      nodesToDelete.add(child)
-      // Remove row index property from the node
-      GridPane.setRowIndex(child, null)
-      GridPane.setColumnIndex(child, null)
-    } else if (childRowIndex > rowIndex) {
-      GridPane.setRowIndex(child, childRowIndex - 1)
-    }
+	val childRowIndex = GridPane.getRowIndex(child) ?: 0
+	if (childRowIndex == rowIndex) {
+	  nodesToDelete.add(child)
+	  // Remove row index property from the node
+	  GridPane.setRowIndex(child, null)
+	  GridPane.setColumnIndex(child, null)
+	} else if (childRowIndex > rowIndex) {
+	  GridPane.setRowIndex(child, childRowIndex - 1)
+	}
   }
   children.removeAll(nodesToDelete)
   return rowIndex
@@ -170,8 +167,8 @@ fun GridPane.removeRow(node: Node): Int {
 
 fun GridPane.removeAllRows() {
   children.forEach {
-    GridPane.setRowIndex(it, null)
-    GridPane.setColumnIndex(it, null)
+	GridPane.setRowIndex(it, null)
+	GridPane.setColumnIndex(it, null)
   }
   children.clear()
   properties.remove(GridPaneRowIdKey)
@@ -182,7 +179,7 @@ fun GridPane.constraintsForColumn(columnIndex: Int) = constraintsFor(columnConst
 fun GridPane.constraintsForRow(rowIndex: Int) = constraintsFor(rowConstraints, rowIndex)
 
 //constraints for row and matt.hurricanefx.tableview.coolColumn can be handled the same way
-internal inline fun <reified T : ConstraintsBase> constraintsFor(constraints: ObservableList<T>, index: Int): T {
+internal inline fun <reified T: ConstraintsBase> constraintsFor(constraints: ObservableList<T>, index: Int): T {
   //    while (constraints.size <= index) constraints.add(T::class.createInstance())
   while (constraints.size <= index) constraints.add(T::class.constructors.first().call())
   return constraints[index]
@@ -190,33 +187,33 @@ internal inline fun <reified T : ConstraintsBase> constraintsFor(constraints: Ob
 
 val Parent.gridpaneColumnConstraints: ColumnConstraints?
   get() {
-    var cursor = this
-    var next = parent
-    while (next != null) {
-      val gridReference = when {
-        next is GridPane    -> next to GridPane.getColumnIndex(cursor)?.let { it }
-        // perhaps we're still in the row builder
-        next.parent == null -> (next.properties[GridPaneParentObjectKey] as? GridPane)?.let {
-          it to next.getChildList()?.indexOf(cursor)
-        }
-        else                -> null
-      }
+	var cursor = this
+	var next = parent
+	while (next != null) {
+	  val gridReference = when {
+		next is GridPane    -> next to GridPane.getColumnIndex(cursor)?.let { it }
+		// perhaps we're still in the row builder
+		next.parent == null -> (next.properties[GridPaneParentObjectKey] as? GridPane)?.let {
+		  it to next.getChildList()?.indexOf(cursor)
+		}
 
-      if (gridReference != null) {
-        val (grid, columnIndex) = gridReference
-        if (columnIndex != null && columnIndex >= 0) return grid.constraintsForColumn(columnIndex)
-      }
-      cursor = next
-      next = next.parent
-    }
-    return null
+		else                -> null
+	  }
+
+	  if (gridReference != null) {
+		val (grid, columnIndex) = gridReference
+		if (columnIndex != null && columnIndex >= 0) return grid.constraintsForColumn(columnIndex)
+	  }
+	  cursor = next
+	  next = next.parent
+	}
+	return null
   }
 
-fun Parent.gridpaneColumnConstraints(op: ColumnConstraints.() -> Unit) = gridpaneColumnConstraints?.apply { op() }
+fun Parent.gridpaneColumnConstraints(op: ColumnConstraints.()->Unit) = gridpaneColumnConstraints?.apply { op() }
 
 
-
-fun EventTarget.toolbar(vararg nodes: Node, op: ToolBar.() -> Unit = {}): ToolBar {
+fun EventTarget.toolbar(vararg nodes: Node, op: ToolBar.()->Unit = {}): ToolBar {
   val toolbar = ToolBar()
   if (nodes.isNotEmpty()) toolbar.items.addAll(nodes)
   opcr(this, toolbar, op)
@@ -229,71 +226,71 @@ fun EventTarget.toolbar(vararg nodes: Node, op: ToolBar.() -> Unit = {}): ToolBa
   ReplaceWith("no children{} wrapper"),
   DeprecationLevel.WARNING
 )
-fun ToolBar.children(op: ToolBar.() -> Unit) = apply { op() }
+fun ToolBar.children(op: ToolBar.()->Unit) = apply { op() }
 
-fun EventTarget.hbox(spacing: Number? = null, alignment: Pos? = null, op: HBox.() -> Unit = {}): HBox {
+fun EventTarget.hbox(spacing: Number? = null, alignment: Pos? = null, op: HBox.()->Unit = {}): HBox {
   val hbox = HBox()
   if (alignment != null) hbox.alignment = alignment
   if (spacing != null) hbox.spacing = spacing.toDouble()
   return opcr(this, hbox, op)
 }
 
-fun EventTarget.vbox(spacing: Number? = null, alignment: Pos? = null, op: VBox.() -> Unit = {}): VBox {
+fun EventTarget.vbox(spacing: Number? = null, alignment: Pos? = null, op: VBox.()->Unit = {}): VBox {
   val vbox = VBox()
   if (alignment != null) vbox.alignment = alignment
   if (spacing != null) vbox.spacing = spacing.toDouble()
   return opcr(this, vbox, op)
 }
 
-fun ToolBar.separator(orientation: Orientation = Orientation.HORIZONTAL, op: Separator.() -> Unit = {}): Separator {
+fun ToolBar.separator(orientation: Orientation = Orientation.HORIZONTAL, op: Separator.()->Unit = {}): Separator {
   val separator = Separator(orientation).also(op)
   add(separator)
   return separator
 }
 
-fun EventTarget.separator(orientation: Orientation = Orientation.HORIZONTAL, op: Separator.() -> Unit = {}) =
+fun EventTarget.separator(orientation: Orientation = Orientation.HORIZONTAL, op: Separator.()->Unit = {}) =
   opcr(this, Separator(orientation), op)
 
-fun EventTarget.group(initialChildren: Iterable<Node>? = null, op: Group.() -> Unit = {}) =
+fun EventTarget.group(initialChildren: Iterable<Node>? = null, op: Group.()->Unit = {}) =
   opcr(this, Group().apply { if (initialChildren != null) children.addAll(initialChildren) }, op)
 
-fun EventTarget.stackpane(initialChildren: Iterable<Node>? = null, op: StackPane.() -> Unit = {}) =
+fun EventTarget.stackpane(initialChildren: Iterable<Node>? = null, op: StackPane.()->Unit = {}) =
   opcr(this, StackPane().apply { if (initialChildren != null) children.addAll(initialChildren) }, op)
 
-fun EventTarget.gridpane(op: GridPane.() -> Unit = {}) = opcr(this, GridPane(), op)
-fun EventTarget.pane(op: Pane.() -> Unit = {}) = opcr(this, Pane(), op)
-fun EventTarget.flowpane(op: FlowPane.() -> Unit = {}) = opcr(this, FlowPane(), op)
-fun EventTarget.tilepane(op: TilePane.() -> Unit = {}) = opcr(this, TilePane(), op)
-fun EventTarget.borderpane(op: BorderPane.() -> Unit = {}) = opcr(this, BorderPane(), op)
+fun EventTarget.gridpane(op: GridPane.()->Unit = {}) = opcr(this, GridPane(), op)
+fun EventTarget.pane(op: Pane.()->Unit = {}) = opcr(this, Pane(), op)
+fun EventTarget.flowpane(op: FlowPane.()->Unit = {}) = opcr(this, FlowPane(), op)
+fun EventTarget.tilepane(op: TilePane.()->Unit = {}) = opcr(this, TilePane(), op)
+fun EventTarget.borderpane(op: BorderPane.()->Unit = {}) = opcr(this, BorderPane(), op)
 
 
 @Deprecated("Use top = node {} instead")
-fun <T : Node> BorderPane.top(topNode: T, op: T.() -> Unit = {}): T {
+fun <T: Node> BorderPane.top(topNode: T, op: T.()->Unit = {}): T {
   top = topNode
   return opcr(this, topNode, op)
 }
 
 
 @Deprecated("Use bottom = node {} instead")
-fun <T : Node> BorderPane.bottom(bottomNode: T, op: T.() -> Unit = {}): T {
+fun <T: Node> BorderPane.bottom(bottomNode: T, op: T.()->Unit = {}): T {
   bottom = bottomNode
   return opcr(this, bottomNode, op)
 }
 
 @Deprecated("Use left = node {} instead")
-fun <T : Node> BorderPane.left(leftNode: T, op: T.() -> Unit = {}): T {
+fun <T: Node> BorderPane.left(leftNode: T, op: T.()->Unit = {}): T {
   left = leftNode
   return opcr(this, leftNode, op)
 }
 
 @Deprecated("Use right = node {} instead")
-fun <T : Node> BorderPane.right(rightNode: T, op: T.() -> Unit = {}): T {
+fun <T: Node> BorderPane.right(rightNode: T, op: T.()->Unit = {}): T {
   right = rightNode
   return opcr(this, rightNode, op)
 }
 
 @Deprecated("Use center = node {} instead")
-fun <T : Node> BorderPane.center(centerNode: T, op: T.() -> Unit = {}): T {
+fun <T: Node> BorderPane.center(centerNode: T, op: T.()->Unit = {}): T {
   center = centerNode
   return opcr(this, centerNode, op)
 }
@@ -302,7 +299,7 @@ fun EventTarget.titledpane(
   title: String? = null,
   node: Node? = null,
   collapsible: Boolean = true,
-  op: (TitledPane).() -> Unit = {}
+  op: (TitledPane).()->Unit = {}
 ): TitledPane {
   val titledPane = TitledPane(title, node)
   titledPane.isCollapsible = collapsible
@@ -314,7 +311,7 @@ fun EventTarget.titledpane(
   title: ObservableValue<String>,
   node: Node? = null,
   collapsible: Boolean = true,
-  op: (TitledPane).() -> Unit = {}
+  op: (TitledPane).()->Unit = {}
 ): TitledPane {
   val titledPane = TitledPane("", node)
   titledPane.textProperty().bind(title)
@@ -323,7 +320,7 @@ fun EventTarget.titledpane(
   return titledPane
 }
 
-fun EventTarget.pagination(pageCount: Int? = null, pageIndex: Int? = null, op: Pagination.() -> Unit = {}): Pagination {
+fun EventTarget.pagination(pageCount: Int? = null, pageIndex: Int? = null, op: Pagination.()->Unit = {}): Pagination {
   val pagination = Pagination()
   if (pageCount != null) pagination.pageCount = pageCount
   if (pageIndex != null) pagination.currentPageIndex = pageIndex
@@ -333,7 +330,7 @@ fun EventTarget.pagination(pageCount: Int? = null, pageIndex: Int? = null, op: P
 fun EventTarget.scrollpane(
   fitToWidth: Boolean = false,
   fitToHeight: Boolean = false,
-  op: ScrollPane.() -> Unit = {}
+  op: ScrollPane.()->Unit = {}
 ): ScrollPane {
   val pane = ScrollPane()
   pane.isFitToWidth = fitToWidth
@@ -346,12 +343,12 @@ fun EventTarget.scrollpane(
 fun EventTarget.splitpane(
   orientation: Orientation = Orientation.HORIZONTAL,
   vararg nodes: Node,
-  op: SplitPane.() -> Unit = {}
+  op: SplitPane.()->Unit = {}
 ): SplitPane {
   val splitpane = SplitPane()
   splitpane.orientation = orientation
   if (nodes.isNotEmpty())
-    splitpane.items.addAll(nodes)
+	splitpane.items.addAll(nodes)
   opcr(this, splitpane, op)
   return splitpane
 }
@@ -361,30 +358,30 @@ fun EventTarget.splitpane(
   ReplaceWith("no items{} wrapper"),
   DeprecationLevel.WARNING
 )
-fun SplitPane.items(op: (SplitPane.() -> Unit)) = op(this)
+fun SplitPane.items(op: (SplitPane.()->Unit)) = op(this)
 
-fun EventTarget.canvas(width: Double = 0.0, height: Double = 0.0, op: Canvas.() -> Unit = {}) =
+fun EventTarget.canvas(width: Double = 0.0, height: Double = 0.0, op: Canvas.()->Unit = {}) =
   opcr(this, Canvas(width, height), op)
 
-fun EventTarget.anchorpane(vararg nodes: Node, op: AnchorPane.() -> Unit = {}): AnchorPane {
+fun EventTarget.anchorpane(vararg nodes: Node, op: AnchorPane.()->Unit = {}): AnchorPane {
   val anchorpane = AnchorPane()
   if (nodes.isNotEmpty()) anchorpane.children.addAll(nodes)
   opcr(this, anchorpane, op)
   return anchorpane
 }
 
-fun EventTarget.accordion(vararg panes: TitledPane, op: Accordion.() -> Unit = {}): Accordion {
+fun EventTarget.accordion(vararg panes: TitledPane, op: Accordion.()->Unit = {}): Accordion {
   val accordion = Accordion()
   if (panes.isNotEmpty()) accordion.panes.addAll(panes)
   opcr(this, accordion, op)
   return accordion
 }
 
-fun <T : Node> Accordion.fold(
+fun <T: Node> Accordion.fold(
   title: String? = null,
   node: T,
   expanded: Boolean = false,
-  op: T.() -> Unit = {}
+  op: T.()->Unit = {}
 ): TitledPane {
   val fold = TitledPane(title, node)
   fold.isExpanded = expanded
@@ -398,14 +395,14 @@ fun <T : Node> Accordion.fold(
   ReplaceWith("Accordion.fold(title, node, op)"),
   DeprecationLevel.WARNING
 )
-fun Accordion.fold(title: String? = null, op: Pane.() -> Unit = {}): TitledPane {
+fun Accordion.fold(title: String? = null, op: Pane.()->Unit = {}): TitledPane {
   val vbox = VBox().also(op)
   val fold = TitledPane(title, if (vbox.children.size == 1) vbox.children[0] else vbox)
   panes += fold
   return fold
 }
 
-fun EventTarget.region(op: Region.() -> Unit = {}) = opcr(this, Region(), op)
+fun EventTarget.region(op: Region.()->Unit = {}) = opcr(this, Region(), op)
 
 
 @Deprecated("Use the paddingRight property instead", ReplaceWith("paddingRight = p"))
@@ -416,7 +413,7 @@ fun Region.paddingRight(p: Double) {
 var Region.paddingRight: Number
   get() = padding.right
   set(value) {
-    padding = padding.copy(right = value.toDouble())
+	padding = padding.copy(right = value.toDouble())
   }
 
 @Deprecated("Use the paddingLeft property instead", ReplaceWith("paddingLeft = p"))
@@ -427,7 +424,7 @@ fun Region.paddingLeft(p: Double) {
 var Region.paddingLeft: Number
   get() = padding.left
   set(value) {
-    padding = padding.copy(left = value)
+	padding = padding.copy(left = value)
   }
 
 @Deprecated("Use the paddingTop property instead", ReplaceWith("paddingTop = p"))
@@ -438,7 +435,7 @@ fun Region.paddingTop(p: Double) {
 var Region.paddingTop: Number
   get() = padding.top
   set(value) {
-    padding = padding.copy(top = value)
+	padding = padding.copy(top = value)
   }
 
 @Deprecated("Use the paddingBottom property instead", ReplaceWith("paddingBottom = p"))
@@ -449,7 +446,7 @@ fun Region.paddingBottom(p: Double) {
 var Region.paddingBottom: Number
   get() = padding.bottom
   set(value) {
-    padding = padding.copy(bottom = value)
+	padding = padding.copy(bottom = value)
   }
 
 @Deprecated("Use the paddingVertical property instead", ReplaceWith("paddingVertical = p"))
@@ -458,10 +455,10 @@ fun Region.paddingVertical(p: Double) {
 }
 
 var Region.paddingVertical: Number
-  get() = padding.vertical * 2
+  get() = padding.vertical*2
   set(value) {
-    val half = value.toDouble() / 2.0
-    padding = padding.copy(vertical = half)
+	val half = value.toDouble()/2.0
+	padding = padding.copy(vertical = half)
   }
 
 @Deprecated("Use the paddingHorizontal property instead", ReplaceWith("paddingHorizontal = p"))
@@ -470,10 +467,10 @@ fun Region.paddingHorizontal(p: Double) {
 }
 
 var Region.paddingHorizontal: Number
-  get() = padding.horizontal * 2
+  get() = padding.horizontal*2
   set(value) {
-    val half = value.toDouble() / 2.0
-    padding = padding.copy(horizontal = half)
+	val half = value.toDouble()/2.0
+	padding = padding.copy(horizontal = half)
   }
 
 @Deprecated("Use the paddingAll property instead", ReplaceWith("paddingAll = p"))
@@ -484,20 +481,20 @@ fun Region.paddingAll(p: Double) {
 var Region.paddingAll: Number
   get() = padding.all
   set(value) {
-    padding = insets(value)
+	padding = insets(value)
   }
 
 fun Region.fitToParentHeight() {
   val parent = this.parent
   if (parent != null && parent is Region) {
-    fitToHeight(parent)
+	fitToHeight(parent)
   }
 }
 
 fun Region.fitToParentWidth() {
   val parent = this.parent
   if (parent != null && parent is Region) {
-    fitToWidth(parent)
+	fitToWidth(parent)
   }
 }
 
@@ -520,26 +517,25 @@ fun Region.fitToSize(region: Region) {
 }
 
 
-
 val Region.paddingVerticalProperty: DoubleProperty
   get() = properties.getOrPut("paddingVerticalProperty") {
-    proxypropDouble(paddingProperty(), { paddingVertical.toDouble() }) {
-      val half = it / 2.0
-      Insets(half, value.right, half, value.left)
-    }
+	proxypropDouble(paddingProperty(), { paddingVertical.toDouble() }) {
+	  val half = it/2.0
+	  Insets(half, value.right, half, value.left)
+	}
   } as DoubleProperty
 
 val Region.paddingHorizontalProperty: DoubleProperty
   get() = properties.getOrPut("paddingHorizontalProperty") {
-    proxypropDouble(paddingProperty(), { paddingHorizontal.toDouble() }) {
-      val half = it / 2.0
-      Insets(value.top, half, value.bottom, half)
-    }
+	proxypropDouble(paddingProperty(), { paddingHorizontal.toDouble() }) {
+	  val half = it/2.0
+	  Insets(value.top, half, value.bottom, half)
+	}
   } as DoubleProperty
 
 val Region.paddingAllProperty: DoubleProperty
   get() = properties.getOrPut("paddingAllProperty") {
-    proxypropDouble(paddingProperty(), { paddingAll.toDouble() }) {
-      Insets(it, it, it, it)
-    }
+	proxypropDouble(paddingProperty(), { paddingAll.toDouble() }) {
+	  Insets(it, it, it, it)
+	}
   } as DoubleProperty
