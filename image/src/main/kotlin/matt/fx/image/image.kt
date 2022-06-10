e3a701933dcba123814d47788e44f4a58bf1b2a6
mgroth0 <mgroth49@gmail.com>
2022-06-10 05:54:36 -0400

    autocommit

--- image/src/main/kotlin/matt/fx/image/image.kt
+++ image/src/main/kotlin/matt/fx/image/image.kt
@@ -2,6 +2,7 @@ package matt.fx.image
 
 import javafx.embed.swing.SwingFXUtils
 import javafx.scene.image.Image
+import matt.klib.file.MFile
 
 import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
@@ -19,7 +20,7 @@ fun jswingIconToImage(jswingIcon: javax.swing.Icon): Image? {
 fun Image.toBufferedImage(): BufferedImage = SwingFXUtils.fromFXImage(this, null)
 fun BufferedImage.toFXImage(): Image = SwingFXUtils.toFXImage(this, null)
 
-//fun Image.save(file: MFile): MFile {
-//  ImageIO.write(toBufferedImage(), file.extension, file)
-//  return file
-//}
+fun Image.save(file: MFile): MFile {
+  ImageIO.write(toBufferedImage(), file.extension, file)
+  return file
+}
