1b9c4556f5e38e87e1d29679cfc01bb9d576c95f
mgroth0 <mgroth49@gmail.com>
2022-06-10 02:58:52 -0400

    autocommit

--- graphics/src/main/kotlin/matt/fx/graphics/clip/clip.kt
+++ graphics/src/main/kotlin/matt/fx/graphics/clip/clip.kt
@@ -1,8 +1,17 @@
 package matt.fx.graphics.clip
 
+import javafx.scene.Cursor
+import javafx.scene.Node
+import javafx.scene.SnapshotParameters
 import javafx.scene.input.Clipboard
 import javafx.scene.input.ClipboardContent
 import javafx.scene.input.DataFormat
+import javafx.scene.input.MouseEvent
+import javafx.scene.input.TransferMode
+import javafx.scene.paint.Color
+import matt.hurricanefx.save
+import matt.klib.commons.TEMP_DIR
+import matt.klib.commons.get
 import matt.klib.file.MFile
 
 
@@ -36,3 +45,79 @@ fun Clipboard.setContent(op: ClipboardContent.() -> Unit) {
 fun Clipboard.putString(value: String) = setContent { putString(value) }
 fun Clipboard.putFiles(files: MutableList<MFile>) = setContent { putFiles(files.map { it.userFile }) }
 fun Clipboard.put(dataFormat: DataFormat, value: Any) = setContent { put(dataFormat, value) }
+
+
+fun Node.drags(file: MFile) {
+  setOnDragDetected {
+    val db = startDragAndDrop(*TransferMode.ANY)
+    db.putFiles(mutableListOf(file))
+    db.dragView = this.snapshot(SnapshotParameters(), null)
+    it.consume()
+  }
+}
+
+fun Node.dragsSnapshot(fill: Color = Color.BLACK) {
+  addEventFilter(MouseEvent.DRAG_DETECTED) {
+    println("drag detected")
+    val params = SnapshotParameters()
+    params.fill = fill
+    val snapshot = snapshot(params, null)
+    val imgFile = snapshot.save(TEMP_DIR["drag_image.png"])
+    val db = startDragAndDrop(*TransferMode.ANY)
+    db.put(DataFormat.FILES, mutableListOf(imgFile))
+    it.consume()
+    println("drag consumed")
+  }
+}
+
+
+
+var dummyDragBoard: Any? = null
+const val DUMMY_TEXT = "DUMMY TEXT"
+
+fun Node.easyDrag(data: Any, getSnapshotNode: ()->Node? = { null }) = easyDrag({ true }, { data }, getSnapshotNode)
+
+fun Node.easyDrag(condition: ()->Boolean = { true }, getData: ()->Any, getSnapshotNode: ()->Node? = { null }) {
+  this.cursor = Cursor.DEFAULT /*just never change it please*/
+  setOnDragDone {
+    this.cursor = Cursor.DEFAULT /*just never change it please*/
+    dummyDragBoard = null
+    it.consume()
+  }
+  setOnDragDetected {
+    this.cursor = Cursor.DEFAULT /*just never change it please*/
+    if (condition()) {
+      val params = SnapshotParameters()
+      params.fill = Color.TRANSPARENT
+      val db = startDragAndDrop(TransferMode.MOVE)
+      val snapNode = getSnapshotNode() ?: this
+      db.dragView = snapNode.snapshot(params, null)
+      db.put(DataFormat.PLAIN_TEXT, DUMMY_TEXT)
+      dummyDragBoard = getData()
+      it.consume()
+    }
+  }
+}
+
+fun Node.easyDrop(handler: ((Any)->Unit)) {
+  this.cursor = Cursor.DEFAULT /*just never change it please*/
+  setOnDragEntered {
+    this.cursor = Cursor.DEFAULT /*just never change it please*/
+    /*it.acceptTransferModes(*TransferMode.ANY)*/
+    it.consume()
+  }
+  setOnDragOver {
+    it.acceptTransferModes(TransferMode.MOVE)
+    this.cursor = Cursor.DEFAULT /*just never change it please*/
+    it.consume()
+  }
+  setOnDragDropped {
+    this.cursor = Cursor.DEFAULT /*just never change it please*/
+    /*if (it.dragboard.getContent(DataFormat.PLAIN_TEXT) == DUMMY_TEXT) {*/
+    handler(dummyDragBoard!!)
+    dummyDragBoard = null
+    it.isDropCompleted = true
+    it.consume()
+    /*}*/
+  }
+}
