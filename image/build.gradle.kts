4d8b1548d236eb28fa7bb54bb406ed9dfdf0374a
mgroth0 <mgroth49@gmail.com>
2022-06-12 16:17:04 -0400

    autocommit

--- image/build.gradle.kts
+++ image/build.gradle.kts
@@ -1,9 +1,14 @@
 modtype = LIB
 
+todo("this should be implementations")
+apis(
+  project(":k:klib")
+)
+
 dependencies {
   //  implementation(projects.kj.kjlib.lang)
   implementation(libs.fx.swing)
   //  implementation(projects.kj.kjlib)
   //  implementation(projects.k.kl.)
-  projectOrLocalMavenJVM("implementation", ":k:klib")
+//  projectOrLocalMavenJVM("implementation", ":k:klib")
 }
