0f7f2ed083474f50b00d4325d881cb007b074118
mgroth0 <mgroth49@gmail.com>
2022-06-20 14:50:41 -0400

    autocommit

--- node/perfdiagram/src/main/kotlin/matt/fx/node/perfdiagram/perfdiagram.kt
+++ node/perfdiagram/src/main/kotlin/matt/fx/node/perfdiagram/perfdiagram.kt
@@ -3,7 +3,7 @@ package matt.fx.node.perfdiagram
 import matt.async.date.Stopwatch
 import matt.fx.web.WebViewPane
 import matt.klib.math.MILLION
-import org.intellij.lang.annotations.Language
+import matt.klib.sankey.sankeyHTML
 import kotlin.math.roundToInt
 
 
@@ -25,48 +25,48 @@ fun Stopwatch.analysisNode(): WebViewPane {
 }
 
 
-@Language("HTML")
-fun sankeyHTML(code: String) = """
-  <html>
-    <head>
-      <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-      <script type="text/javascript">
-      ${sankeyJS(code)}
-      </script>
-    </head>
-    <body>
-      <div id="sankey_basic" style="width: 900px; height: 300px;"></div>
-    </body>
-  </html>
-""".trimIndent()
-
-@Language("JavaScript")
-fun sankeyJS(code: String) = """
-     google.charts.matt.gui.ser.load('current', {'packages':['sankey']});
-        google.charts.setOnLoadCallback(drawChart);
-
-        function drawChart() {
-          var data = new google.visualization.DataTable();
-          data.addColumn('string', 'From');
-          data.addColumn('string', 'To');
-          data.addColumn('number', 'Weight');
-          data.addRows([
-//            [ 'A', 'X', 5.3 ],
-//            [ 'A', 'Y', 7 ],
-//            [ 'A', 'Z', 6 ],
-//            [ 'B', 'X', 2 ],
-//            [ 'B', 'Y', 9 ],
-//            [ 'B', 'Z', 4 ],
-            ${code}
-          ]);
-
-          // Sets chart options.
-          var options = {
-            width: 600,
-          };
-
-          // Instantiates and draws our chart, passing in some options.
-          var chart = new google.visualization.Sankey(document.getElementById('sankey_basic'));
-          chart.draw(data, options);
-        }
-""".trimIndent()
+//@Language("HTML")
+//fun sankeyHTML(code: String) = """
+//  <html>
+//    <head>
+//      <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+//      <script type="text/javascript">
+//      ${sankeyJS(code)}
+//      </script>
+//    </head>
+//    <body>
+//      <div id="sankey_basic" style="width: 900px; height: 300px;"></div>
+//    </body>
+//  </html>
+//""".trimIndent()
+//
+//@Language("JavaScript")
+//fun sankeyJS(code: String) = """
+//     google.charts.matt.gui.ser.load('current', {'packages':['sankey']});
+//        google.charts.setOnLoadCallback(drawChart);
+//
+//        function drawChart() {
+//          var data = new google.visualization.DataTable();
+//          data.addColumn('string', 'From');
+//          data.addColumn('string', 'To');
+//          data.addColumn('number', 'Weight');
+//          data.addRows([
+////            [ 'A', 'X', 5.3 ],
+////            [ 'A', 'Y', 7 ],
+////            [ 'A', 'Z', 6 ],
+////            [ 'B', 'X', 2 ],
+////            [ 'B', 'Y', 9 ],
+////            [ 'B', 'Z', 4 ],
+//            ${code}
+//          ]);
+//
+//          // Sets chart options.
+//          var options = {
+//            width: 600,
+//          };
+//
+//          // Instantiates and draws our chart, passing in some options.
+//          var chart = new google.visualization.Sankey(document.getElementById('sankey_basic'));
+//          chart.draw(data, options);
+//        }
+//""".trimIndent()
