8bff5e2cc776b38852d25f24b1fc2ab0b1758717
mgroth0 <mgroth49@gmail.com>
2022-06-20 17:03:16 -0400

    autocommit

--- node/perfdiagram/src/main/kotlin/matt/fx/node/perfdiagram/perfdiagram.kt
+++ node/perfdiagram/src/main/kotlin/matt/fx/node/perfdiagram/perfdiagram.kt
@@ -8,8 +8,6 @@ import kotlin.math.roundToInt
 
 fun Stopwatch.analysisNode() = StopWatchAnalysisNode(this)
 
-class SoStupid
-
 class StopWatchAnalysisNode(val sw: Stopwatch): WebViewPane(sankeyHTML(sw.increments().joinToString("\n") {
   "[ '${sw.prefix}', '${it.second}', ${(it.first/MILLION).roundToInt()} ],"
 }))
