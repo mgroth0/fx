62df09a8d0aaa73a9ec5d643c48c35a8a476435b
Matthew Groth <matthewgroth@Matthews-MacBook-Pro.local>
2022-07-29 15:39:43 -0400

    autocommit

--- node/inspect/src/main/kotlin/matt/fx/node/inspect/inspect.kt
+++ node/inspect/src/main/kotlin/matt/fx/node/inspect/inspect.kt
@@ -12,11 +12,11 @@ import matt.hurricanefx.tornadofx.control.label
 import matt.hurricanefx.tornadofx.nodes.add
 import matt.hurricanefx.tornadofx.nodes.clear
 import matt.hurricanefx.tornadofx.nodes.removeFromParent
+import matt.hurricanefx.wrapper.control.list.ListViewWrapper
+import matt.hurricanefx.wrapper.control.table.TableViewWrapper
 import matt.hurricanefx.wrapper.pane.box.BoxWrapper
 import matt.hurricanefx.wrapper.pane.hbox.HBoxWrapper
-import matt.hurricanefx.wrapper.ListViewWrapper
 import matt.hurricanefx.wrapper.pane.PaneWrapper
-import matt.hurricanefx.wrapper.TableViewWrapper
 import matt.hurricanefx.wrapper.pane.vbox.VBoxWrapper
 
 
