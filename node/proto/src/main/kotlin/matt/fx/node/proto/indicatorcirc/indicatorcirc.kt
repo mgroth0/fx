02f8e84a4a1105dfbd023b573b6e667f696eeb4c
mgroth0 <mgroth49@gmail.com>
2022-09-04 19:32:04 -0400

    autocommit

--- node/proto/src/main/kotlin/matt/fx/node/proto/indicatorcirc/indicatorcirc.kt
+++ node/proto/src/main/kotlin/matt/fx/node/proto/indicatorcirc/indicatorcirc.kt
@@ -1,11 +1,11 @@
 package matt.fx.node.proto.indicatorcirc
 
-import javafx.beans.property.BooleanProperty
+import javafx.beans.value.ObservableValue
 import javafx.scene.paint.Color
 import matt.hurricanefx.eye.prop.objectBinding
 import matt.hurricanefx.wrapper.shape.circle.CircleWrapper
 
-fun indicatorCircle(booleanProperty: BooleanProperty) = CircleWrapper(8.0).apply {
+fun indicatorCircle(booleanProperty: ObservableValue<Boolean>) = CircleWrapper(8.0).apply {
   fillProperty().bind(booleanProperty.objectBinding {
 	val colo = if (it == true) Color.LIGHTGREEN else Color.DARKRED
 	//	val colo = if (it == true) null else Color.DARKRED
