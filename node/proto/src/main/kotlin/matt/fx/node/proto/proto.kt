7a7a5910295c93aaaaa01ed10b562cae5366e50f
mgroth0 <mgroth49@gmail.com>
2022-09-26 23:42:45 -0400

    autocommit

--- node/proto/src/main/kotlin/matt/fx/node/proto/proto.kt
+++ node/proto/src/main/kotlin/matt/fx/node/proto/proto.kt
@@ -1,9 +1,42 @@
 package matt.fx.node.proto
 
+import javafx.scene.canvas.GraphicsContext
+import matt.fx.graphics.icon.ICON_HEIGHT
+import matt.fx.graphics.icon.ICON_WIDTH
+import matt.fx.graphics.icon.Icon
+import matt.fx.graphics.icon.IconImage
+import matt.hurricanefx.wrapper.canvas.CanvasWrapper
 import matt.hurricanefx.wrapper.node.NodeWrapper
+import matt.hurricanefx.wrapper.node.NodeWrapperImpl
 import matt.hurricanefx.wrapper.pane.vbox.VBoxWrapper
+import matt.model.dir.Direction
+import matt.model.dir.Direction.BACKWARD
+import matt.model.dir.Direction.FORWARD
 
 fun iconSpacer() = VBoxWrapper<NodeWrapper>().apply {
   exactHeight = 20.0
   exactWidth = 5.0
 }
+
+
+fun Direction.graphic(): NodeWrapperImpl<*>? {
+  return if (this == FORWARD) {
+    Icon("white/forward")
+  } else if (this == BACKWARD) {
+
+    val canvas = CanvasWrapper(ICON_WIDTH, ICON_HEIGHT)
+    val image = IconImage("white/forward")
+    val xoff = 0.0 /*15.0*/
+    val gc: GraphicsContext = canvas.graphicsContext
+    gc.save()
+    gc.translate(image.width + xoff*2, 0.0)
+    gc.scale(-1.0, 1.0)
+    gc.drawImage(image, xoff, 0.0)
+    gc.restore()
+    gc.drawImage(image, xoff, 0.0)
+
+    canvas
+  } else null
+}
+
+
