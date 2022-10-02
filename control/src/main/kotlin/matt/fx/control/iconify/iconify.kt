99d5f4d15f692cd1c198dd813539ee52109b65db
mgroth0 <mgroth49@gmail.com>
2022-10-02 12:15:07 -0400

    autocommit

--- control/src/main/kotlin/matt/fx/control/iconify/iconify.kt
+++ control/src/main/kotlin/matt/fx/control/iconify/iconify.kt
@@ -9,6 +9,8 @@ import matt.fx.graphics.icon.Icon
 import matt.fx.graphics.style.DarkModeController
 import matt.fx.control.win.interact.WinGeom
 import matt.fx.control.win.interact.WinOwn
+import matt.fx.control.win.interact.openInNewWindow
+import matt.fx.graphics.core.scene.reloadStyle
 import matt.fx.graphics.win.winfun.noDocking
 import matt.fx.graphics.wrapper.node.setOnDoubleClick
 import matt.fx.graphics.wrapper.scene.SceneWrapper
