32a178b3c75fdde0dc5e0bb00fcc13245a9802b3
mgroth0 <mgroth49@gmail.com>
2022-10-04 00:30:27 -0400

    autocommit

--- control/src/main/kotlin/matt/fx/control/wrapper/control/spinner/spinner.kt
+++ control/src/main/kotlin/matt/fx/control/wrapper/control/spinner/spinner.kt
@@ -1,10 +1,6 @@
 package matt.fx.control.wrapper.control.spinner
 
-import javafx.beans.property.BooleanProperty
-import javafx.beans.property.Property
-import javafx.beans.property.ReadOnlyObjectProperty
 import javafx.beans.value.ObservableValue
-import javafx.collections.ObservableList
 import javafx.scene.control.Spinner
 import javafx.scene.control.SpinnerValueFactory
 import matt.fx.control.wrapper.control.ControlWrapperImpl
@@ -12,154 +8,155 @@ import matt.fx.graphics.wrapper.ET
 import matt.fx.graphics.wrapper.node.NodeWrapper
 import matt.fx.graphics.wrapper.node.attachTo
 import matt.hurricanefx.eye.bind.smartBind
-import matt.hurricanefx.eye.mtofx.createWritableFXPropWrapper
+import matt.hurricanefx.eye.wrapper.obs.collect.createFXWrapper
+import matt.hurricanefx.eye.wrapper.obs.obsval.prop.toNonNullableProp
+import matt.hurricanefx.eye.wrapper.obs.obsval.prop.toNullableProp
+import matt.hurricanefx.eye.wrapper.obs.obsval.toNonNullableROProp
 import matt.lang.err
+import matt.obs.col.olist.ObsList
 import matt.obs.prop.BindableProperty
+import matt.obs.prop.Var
 
 
 /**
  * Create a spinner for an arbitrary type. This spinner requires you to configure a value factory, or it will throw an exception.
  */
-fun <T> ET.spinner(
+/*MATT NOTE: SPINNER INTERNALS USES NULL IN VARIOUS MECHANISMS. SPINNER TYPES CANNOT BE NULLABLE.*/
+fun <T: Any> ET.spinner(
   editable: Boolean = false,
-  property: Property<T>? = null,
+  property: Var<T>? = null,
   enableScroll: Boolean = false,
   op: SpinnerWrapper<T>.()->Unit = {}
 ) = SpinnerWrapper<T>().also {
-  it.isEditable = editable
   it.attachTo(this, op)
 
   if (property != null) requireNotNull(it.valueFactory) {
 	"You must configure the value factory or use the Number based spinner builder " +
 		"which configures a default value factory along with min, max and initialValue!"
   }.valueProperty().apply {
-	bindBidirectional(property)
+	toNonNullableProp().bindBidirectional(property)
   }
-
-  if (enableScroll) it.setOnScroll { event ->
-	if (event.deltaY > 0) it.increment()
-	if (event.deltaY < 0) it.decrement()
-  }
-
-  if (editable) it.focusedProperty().addListener { _, _, newValue: Boolean? ->
-	if (newValue == null) err("here it is")
-	if (!newValue) it.increment(0)
-  }
+  it.initialConfig(
+	editable = editable,
+	enableScroll = enableScroll
+  )
 }
 
+
+/*MATT NOTE: SPINNER INTERNALS USES NULL IN VARIOUS MECHANISMS. SPINNER TYPES CANNOT BE NULLABLE.*/
 inline fun <reified T: Number> ET.spinner(
   min: T? = null,
   max: T? = null,
   initialValue: T? = null,
   amountToStepBy: T? = null,
   editable: Boolean = false,
-  property: BindableProperty<T>? = null,
+  property: BindableProperty<T?>? = null,
   enableScroll: Boolean = false,
   noinline op: SpinnerWrapper<T>.()->Unit = {}
 ): SpinnerWrapper<T> {
-  val spinner: SpinnerWrapper<T>
-
-
   /*property is IntegerProperty && property !is DoubleProperty && property !is FloatProperty) ||*/
   val isInt = min is Int || max is Int || initialValue is Int ||
-	  T::class == kotlin.Int::class || T::class == java.lang.Integer::class || T::class.javaPrimitiveType == java.lang.Integer::class.java
-  if (isInt) {
-	spinner = SpinnerWrapper(
+	  T::class == Int::class || T::class == java.lang.Integer::class || T::class.javaPrimitiveType == java.lang.Integer::class.java
+  val spinner = if (isInt) {
+	SpinnerWrapper<T>(
 	  min?.toInt() ?: 0,
 	  max?.toInt() ?: 100,
 	  initialValue?.toInt() ?: 0,
 	  amountToStepBy?.toInt() ?: 1
 	)
   } else {
-	spinner = SpinnerWrapper(
+	SpinnerWrapper(
 	  min?.toDouble() ?: 0.0, max?.toDouble() ?: 100.0, initialValue?.toDouble()
 		?: 0.0, amountToStepBy?.toDouble() ?: 1.0
 	)
   }
   if (property != null) {
-	spinner.valueFactory!!.valueProperty().bindBidirectional(property.createWritableFXPropWrapper())
+	spinner.valueFactory!!.valueProperty().toNullableProp().bindBidirectional(property)
   }
-  spinner.isEditable = editable
-
-  if (enableScroll) {
-	spinner.setOnScroll { event ->
-	  if (event.deltaY > 0) spinner.increment()
-	  if (event.deltaY < 0) spinner.decrement()
-	}
-  }
-
-  if (editable) {
-	spinner.focusedProperty().addListener { _, _, newValue: Boolean? ->
-	  if (newValue == null) err("here it is")
-	  if (!newValue) {
-		spinner.increment(0)
-	  }
-	}
-  }
+  spinner.initialConfig(
+	editable = editable,
+	enableScroll = enableScroll
+  )
 
   return spinner.attachTo(this, op)
 }
 
-fun <T> ET.spinner(
-  items: ObservableList<T>,
+/*MATT NOTE: SPINNER INTERNALS USES NULL IN VARIOUS MECHANISMS. SPINNER TYPES CANNOT BE NULLABLE.*/
+fun <T: Any> ET.spinner(
+  items: ObsList<T>,
   editable: Boolean = false,
-  property: Property<T>? = null,
+  property: Var<T>? = null,
   enableScroll: Boolean = false,
   op: SpinnerWrapper<T>.()->Unit = {}
-) = SpinnerWrapper<T>(items).attachTo(this, op) {
+) = SpinnerWrapper(items).attachTo(this, op) {
   if (property != null) it.valueFactory!!.valueProperty().apply {
-	bindBidirectional(property)
+	toNonNullableProp().bindBidirectional(property)
   }
-
-  it.isEditable = editable
-
-  if (enableScroll) it.setOnScroll { event ->
-	if (event.deltaY > 0) it.increment()
-	if (event.deltaY < 0) it.decrement()
-  }
-
-  if (editable) it.focusedProperty().addListener { _, _, newValue: Boolean? ->
-	if (newValue == null) err("here it is")
-	if (!newValue) it.increment(0)
-  }
+  it.initialConfig(
+	editable = editable,
+	enableScroll = enableScroll
+  )
 }
 
-fun <T> ET.spinner(
+/*MATT NOTE: SPINNER INTERNALS USES NULL IN VARIOUS MECHANISMS. SPINNER TYPES CANNOT BE NULLABLE.*/
+fun <T: Any> ET.spinner(
   valueFactory: SpinnerValueFactory<T>,
   editable: Boolean = false,
-  property: Property<T>? = null,
+  property: Var<T>? = null,
   enableScroll: Boolean = false,
   op: SpinnerWrapper<T>.()->Unit = {}
-) = SpinnerWrapper<T>(valueFactory).attachTo(this, op) {
+) = SpinnerWrapper(valueFactory).attachTo(this, op) {
   if (property != null) it.valueFactory!!.valueProperty().apply {
-	bindBidirectional(property)
+	toNonNullableProp().bindBidirectional(property)
   }
-
-  it.isEditable = editable
-
-  if (enableScroll) it.setOnScroll { event ->
-	if (event.deltaY > 0) it.increment()
-	if (event.deltaY < 0) it.decrement()
-  }
-
-  if (editable) it.focusedProperty().addListener { _, _, newValue: Boolean? ->
-	if (newValue == null) err("here it is")
-	if (!newValue) it.increment(0)
-  }
+  it.initialConfig(
+	editable = editable,
+	enableScroll = enableScroll
+  )
 }
 
-class SpinnerWrapper<T>(
-   node: Spinner<T> = Spinner<T>(),
+@PublishedApi
+internal fun SpinnerWrapper<*>.initialConfig(
+  editable: Boolean = false,
+  enableScroll: Boolean = false,
+) {
+  isEditable = editable
+
+  if (enableScroll) listenToScrolls()
+
+  if (editable) tfxWeirdEditableThing()
+}
+
+/*MATT NOTE: SPINNER INTERNALS USES NULL IN VARIOUS MECHANISMS. SPINNER TYPES CANNOT BE NULLABLE.*/
+class SpinnerWrapper<T: Any>(
+  node: Spinner<T> = Spinner<T>(),
 ): ControlWrapperImpl<Spinner<T>>(node) {
-  constructor(min: Int, max: Int, initial: Int, step: Int): this(Spinner(min, max, initial, step))
-  constructor(min: Double, max: Double, initialVal: Double, step: Double): this(Spinner(min, max, initialVal, step))
-  constructor(min: Double, max: Double, initialVal: Double): this(Spinner(min, max, initialVal))
-  constructor(items: ObservableList<T>): this(Spinner(items))
+  constructor(
+	min: Int,
+	max: Int,
+	initial: Int,
+	step: Int
+  ): this(Spinner(min, max, initial, step))
+
+  constructor(
+	min: Double,
+	max: Double,
+	initialVal: Double,
+	step: Double
+  ): this(Spinner(min, max, initialVal, step))
+
+  constructor(
+	min: Double,
+	max: Double,
+	initialVal: Double
+  ): this(Spinner(min, max, initialVal))
+
+  constructor(items: ObsList<T>): this(Spinner(items.createFXWrapper()))
   constructor(valueFactory: SpinnerValueFactory<T>): this(Spinner(valueFactory))
 
 
-  val value: T? get() = node.value
-  fun valueProperty(): ReadOnlyObjectProperty<T> = node.valueProperty()
+  val value: T get() = node.value
+  val valueProperty by lazy { node.valueProperty().toNonNullableROProp() }
 
   var valueFactory: SpinnerValueFactory<T>?
 	get() = node.valueFactory
@@ -174,8 +171,14 @@ class SpinnerWrapper<T>(
 	  node.isEditable = value
 	}
 
+  fun listenToScrolls() {
+	setOnScroll { event ->
+	  if (event.deltaY > 0) increment()
+	  if (event.deltaY < 0) decrement()
+	}
+  }
 
-  fun editableProperty(): BooleanProperty = node.editableProperty()
+  val editableProperty by lazy { node.editableProperty().toNonNullableProp() }
 
   fun increment() = node.increment()
   fun decrement() = node.decrement()
@@ -184,7 +187,18 @@ class SpinnerWrapper<T>(
   override fun addChild(child: NodeWrapper, index: Int?) {
 	TODO("Not yet implemented")
   }
+
+  fun tfxWeirdEditableThing() {
+	focusedProperty().addListener { _, _, newValue: Boolean? ->
+	  if (newValue == null) err("here it is")
+	  if (!newValue) increment(0)
+	}
+  }
+
+
+
+
 }
 
-fun <T> SpinnerWrapper<T>.bind(property: ObservableValue<T>, readonly: Boolean = false) =
+fun <T: Any> SpinnerWrapper<T>.bind(property: ObservableValue<T>, readonly: Boolean = false) =
   valueFactory!!.valueProperty().smartBind(property, readonly)
--- graphics/src/main/kotlin/matt/fx/graphics/wrapper/region/border/border.kt
+++ graphics/src/main/kotlin/matt/fx/graphics/wrapper/region/border/border.kt
@@ -13,7 +13,7 @@ fun Paint.solidBorder() = FXBorder.solid(this)
 fun Paint.dashedBorder() = FXBorder.solid(this)
 
 object FXBorder {
-  fun solid(color: Paint) = Border(BorderStroke(color, BorderStrokeStyle.SOLID, null, null))
+  fun solid(color: Paint, widths: Double? = null) = Border(BorderStroke(color, BorderStrokeStyle.SOLID, null, widths?.let { BorderWidths(widths) }))
   fun dashed(color: Paint) = Border(BorderStroke(color, BorderStrokeStyle.DASHED, null, null))
   val EMPTY: Border by lazy { Border.EMPTY }
 }
