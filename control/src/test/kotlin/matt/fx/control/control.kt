b90d33e014ce3225094839c127e83685b9b6a30d
mgroth0 <mgroth49@gmail.com>
2022-10-10 21:30:13 -0400

    autocommit

--- control/src/test/kotlin/matt/fx/control/control.kt
+++ control/src/test/kotlin/matt/fx/control/control.kt
@@ -1,7 +1,8 @@
 package matt.fx.control
 
+import javafx.application.Application
 import javafx.scene.Node
-import javafx.scene.control.ScrollPane
+import javafx.stage.Stage
 import matt.reflect.subclasses
 import matt.test.yesIUseTestLibs
 import org.junit.jupiter.api.Test
@@ -9,21 +10,22 @@ import kotlin.contracts.ExperimentalContracts
 
 class SomeTests {
 
-  @Test
-  @ExperimentalContracts
-  fun testWrappers() {
+  @Test @ExperimentalContracts fun testWrappers() {
 
 	yesIUseTestLibs()
 
-	listOf<Node>(
-	  ScrollPane()
-	)
-
-	Node::class.subclasses().forEach {
-	  println("node subclass: ${it}")
-	}
-
-	//	assert(false)
+	Application.launch(TestApp::class.java)
 
   }
+}
+
+
+class TestApp(): Application() {
+
+  override fun start(primaryStage: Stage?) {
+	Node::class.subclasses().forEach {
+	  println("node subclass: $it")
+	}
+  }
+
 }
