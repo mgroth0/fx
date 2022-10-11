85508a06930f30b7ed625b49432b0bcaa6671bcf
mgroth0 <mgroth49@gmail.com>
2022-10-10 21:06:59 -0400

    autocommit

--- control/src/test/kotlin/matt/fx/control/control.kt
+++ control/src/test/kotlin/matt/fx/control/control.kt
@@ -1,7 +1,9 @@
 package matt.fx.control
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback
 import kotlin.contracts.ExperimentalContracts
+import kotlin.test.junit5.JUnit5Asserter
 
 class SomeTests {
 
@@ -9,6 +11,9 @@ class SomeTests {
   @Test
   @ExperimentalContracts
   fun testWrappers() {
+
+	val yesIUseJUnitHereIsSomethingNotInlined = AfterTestExecutionCallback { TODO("Not yet implemented") }
+	val hereIsAnother = JUnit5Asserter
 
 	assert(false)
 
