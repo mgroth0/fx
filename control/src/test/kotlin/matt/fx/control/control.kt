c600e4e7c423cb9a39e5a9d7d7f1a8e628a8dc97
mgroth0 <mgroth49@gmail.com>
2022-10-10 21:54:33 -0400

    autocommit

--- control/src/test/kotlin/matt/fx/control/control.kt
+++ control/src/test/kotlin/matt/fx/control/control.kt
@@ -4,10 +4,12 @@ import javafx.application.Application
 import javafx.application.Platform
 import javafx.scene.Node
 import javafx.stage.Stage
+import matt.collect.itr.recurse.recurse
 import matt.reflect.subclasses
 import matt.test.yesIUseTestLibs
 import org.junit.jupiter.api.Test
 import kotlin.contracts.ExperimentalContracts
+import kotlin.reflect.KClass
 
 class SomeTests {
 
@@ -23,10 +25,12 @@ class SomeTests {
 }
 
 
-class TestApp(): Application() {
+class TestApp: Application() {
 
   override fun start(primaryStage: Stage?) {
-	Node::class.subclasses().forEach {
+	(Node::class as KClass<out Node>).recurse { it.subclasses() }.filter {
+	  !it.isAbstract
+	}.forEach {
 	  println("node subclass: $it")
 	}
 	primaryStage!!.close()
