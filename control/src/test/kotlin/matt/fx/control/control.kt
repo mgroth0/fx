e16008f3ee13f77773ae508acec20cca1e86cab8
mgroth0 <mgroth49@gmail.com>
2022-10-10 21:11:51 -0400

    autocommit

--- control/src/test/kotlin/matt/fx/control/control.kt
+++ control/src/test/kotlin/matt/fx/control/control.kt
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.extension.AfterTestExecutionCallback
 import kotlin.contracts.ExperimentalContracts
 import kotlin.test.junit5.JUnit5Asserter
 
+@Suppress("UNUSED_VARIABLE")
 class SomeTests {
 
 
   @Test
   @ExperimentalContracts
   fun testWrappers() {
+
 
 	val yesIUseJUnitHereIsSomethingNotInlined = AfterTestExecutionCallback { TODO("Not yet implemented") }
 	val hereIsAnother = JUnit5Asserter
