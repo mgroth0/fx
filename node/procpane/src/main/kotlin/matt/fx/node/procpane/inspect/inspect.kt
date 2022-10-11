fabeb217f0d3d60933b7e83dca4a5f6e57dc9864
mgroth0 <mgroth49@gmail.com>
2022-10-11 11:11:51 -0400

    autocommit

--- node/procpane/src/main/kotlin/matt/fx/node/procpane/inspect/inspect.kt
+++ node/procpane/src/main/kotlin/matt/fx/node/procpane/inspect/inspect.kt
@@ -13,7 +13,6 @@ import matt.fx.graphics.fxthread.ts.nonBlockingFXWatcher
 import matt.fx.graphics.wrapper.node.NodeWrapper
 import matt.fx.graphics.wrapper.pane.hbox.HBoxWrapperImpl
 import matt.hurricanefx.eye.lib.onChange
-import matt.log.debug
 import matt.obs.bind.binding
 import matt.obs.bindings.str.ObsS
 import matt.obs.bindings.str.plus
@@ -59,7 +58,6 @@ class ProcessInspectPane(initialValue: Process?): HBoxWrapperImpl<NodeWrapper>()
 	  prefWidthProperty.bind(this@ProcessInspectPane.widthProperty/2.0)
 	}
 	if (p != null) {
-	  @Suppress("RemoveExplicitTypeArguments")
 	  treetableview<ProcessOrHandleWrapper> {
 		prefWidthProperty.bind(this@ProcessInspectPane.widthProperty/2.0)
 		val col = (column("pid") { cdf: TreeTableColumn.CellDataFeatures<ProcessOrHandleWrapper, String> ->
@@ -70,7 +68,6 @@ class ProcessInspectPane(initialValue: Process?): HBoxWrapperImpl<NodeWrapper>()
 		  prefWidth = 100.0
 		}
 		sortOrder.setAll(col.node) // not working for matt.fx.control.layout.children?
-		debug("update4")
 		(column("main program") { cdf: TreeTableColumn.CellDataFeatures<ProcessOrHandleWrapper, String> ->
 		  cdf.value?.value?.command()?.let { com ->
 			BindableProperty(mFile(com).name)
@@ -79,24 +76,16 @@ class ProcessInspectPane(initialValue: Process?): HBoxWrapperImpl<NodeWrapper>()
 		  prefWidth = 100.0
 		}
 
-		debug("update5")
 		selectionModel.selectedItemProperty.onChange {
-		  debug("select1")
 
 		  this@ProcessInspectPane.propLabel.oProp.set(it?.value)
-		  debug("select2")
 		}
 		root = TreeItem(ProcessOrHandleWrapper(p))
 		root!!.isExpanded = true
-		debug("update6")
 		selectionModel.select(root)
-		debug("update7")
 		isShowRoot = true
-		debug("update7.1")
 		populate { it.value.obsChildren }
-		debug("update8")
 	  }
-	  debug("update9")
 	}
   }
 }
