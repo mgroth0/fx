c6ef0f72642a6d03415a2b1da4d3455e6441df48
mgroth0 <mgroth49@gmail.com>
2022-10-10 22:54:11 -0400

    autocommit

--- control/src/test/kotlin/matt/fx/control/control.kt
+++ control/src/test/kotlin/matt/fx/control/control.kt
@@ -1,9 +1,8 @@
 package matt.fx.control
 
-import javafx.application.Application
 import javafx.application.Platform
 import javafx.scene.Node
-import javafx.stage.Stage
+import matt.fx.control.fxapp.runFXAppBlocking
 import matt.reflect.reflections.subclasses
 import matt.test.yesIUseTestLibs
 import org.junit.jupiter.api.Test
@@ -18,24 +17,20 @@ class SomeTests {
 
 	yesIUseTestLibs()
 
-	Platform.setImplicitExit(true)
-	Application.launch(TestApp::class.java)
+	runFXAppBlocking(
+	  args = arrayOf(),
+	  usePreloaderApp = false,
+	  t = null,
+	  fxOp = {
+		(Node::class as KClass<out Node>).subclasses("javafx").filter {
+		  !it.isAbstract
+		}.forEach {
+		  println("node subclass: $it")
+		}
+		Platform.exit()
+	  }
+	)
 
 
   }
-}
-
-
-class TestApp: Application() {
-
-  override fun start(primaryStage: Stage?) {
-	(Node::class as KClass<out Node>).subclasses("javafx").filter {
-	  !it.isAbstract
-	}.forEach {
-	  println("node subclass: $it")
-	}
-	primaryStage!!.close()
-	Platform.exit()
-  }
-
 }
