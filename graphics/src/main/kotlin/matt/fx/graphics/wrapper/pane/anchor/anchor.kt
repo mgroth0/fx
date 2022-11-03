994a12752b35da93aa55e01002a4f67ea62eecbf
mgroth0 <mgroth49@gmail.com>
2022-11-03 14:43:08 -0400

    autocommit

--- graphics/src/main/kotlin/matt/fx/graphics/wrapper/pane/anchor/anchor.kt
+++ graphics/src/main/kotlin/matt/fx/graphics/wrapper/pane/anchor/anchor.kt
@@ -8,7 +8,6 @@ import matt.fx.graphics.wrapper.node.attach
 import matt.fx.graphics.wrapper.pane.PaneWrapper
 import matt.fx.graphics.wrapper.pane.PaneWrapperImpl
 import matt.lang.NOT_IMPLEMENTED
-import matt.log.warn.warnOnce
 
 fun <C: NodeWrapper> ET.anchorpane(
   vararg nodes: C,
@@ -25,31 +24,34 @@ interface AnchorPaneWrapper<C: NodeWrapper>: PaneWrapper<C> {
   var left: NodeWrapper
 	get() = NOT_IMPLEMENTED
 	set(value) {
+	  /*be very careful changing this line to use wrappers... or just never do it. There used to be horrible issue here and I never found the root cause. Switching it away from using wrappers to using `value.node` and `node.children` completely solved it. Yes, maybe some internal improvement I made since then fixed the issue and maybe switching to wrappers now would fix it. But is it really worth it? If I feel the need to change it, only do so when I have extensive testing set up.*/
 	  if (value.node !in node.children) add(value)
 	  value.setAsLeftAnchor(0.0)
 	}
   var right: NodeWrapper
 	get() = NOT_IMPLEMENTED
 	set(value) {
+	  /*be very careful changing this line to use wrappers... or just never do it. There used to be horrible issue here and I never found the root cause. Switching it away from using wrappers to using `value.node` and `node.children` completely solved it. Yes, maybe some internal improvement I made since then fixed the issue and maybe switching to wrappers now would fix it. But is it really worth it? If I feel the need to change it, only do so when I have extensive testing set up.*/
 	  if (value.node !in node.children) add(value)
 	  value.setAsRightAnchor(0.0)
 	}
   var bottom: NodeWrapper
 	get() = NOT_IMPLEMENTED
 	set(value) {
+	  /*be very careful changing this line to use wrappers... or just never do it. There used to be horrible issue here and I never found the root cause. Switching it away from using wrappers to using `value.node` and `node.children` completely solved it. Yes, maybe some internal improvement I made since then fixed the issue and maybe switching to wrappers now would fix it. But is it really worth it? If I feel the need to change it, only do so when I have extensive testing set up.*/
 	  if (value.node !in node.children) add(value)
 	  value.setAsBottomAnchor(0.0)
 	}
   var top: NodeWrapper
 	get() = NOT_IMPLEMENTED
 	set(value) {
+	  /*be very careful changing this line to use wrappers... or just never do it. There used to be horrible issue here and I never found the root cause. Switching it away from using wrappers to using `value.node` and `node.children` completely solved it. Yes, maybe some internal improvement I made since then fixed the issue and maybe switching to wrappers now would fix it. But is it really worth it? If I feel the need to change it, only do so when I have extensive testing set up.*/
 	  if (value.node !in node.children) add(value)
 	  value.setAsTopAnchor(0.0)
 	}
   var allSides: NodeWrapper
 	get() = NOT_IMPLEMENTED
 	set(value) {
-	  warnOnce("`value.node !in node.children` is a temp fix. There must be some wrapper issue here.")
 	  left = value
 	  right = value
 	  bottom = value
