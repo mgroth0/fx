0e5e95bdd21d49263b5f7d906f82134cdc580ab3
mgroth0 <mgroth49@gmail.com>
2022-12-15 15:48:59 -0500

    autocommit

--- control/src/main/kotlin/matt/fx/control/wrapper/chart/axis/value/number/tickconfig/calcticks/calcticks.kt
+++ control/src/main/kotlin/matt/fx/control/wrapper/chart/axis/value/number/tickconfig/calcticks/calcticks.kt
@@ -1,9 +1,9 @@
 package matt.fx.control.wrapper.chart.axis.value.number.tickconfig.calcticks
 
-import matt.model.data.mathable.MathAndComparable
+import matt.model.data.mathable.NumberWrapper
 
-internal fun <T: MathAndComparable<T>> calcBestTicks(
-  range: MathAndComparable<T>
+internal fun <T: NumberWrapper<T>> calcBestTicks(
+  range: NumberWrapper<T>
 ): T? {
   if (range.isZero || range.isInfinite || range.isNaN) return null
   val absRange = range.abs
