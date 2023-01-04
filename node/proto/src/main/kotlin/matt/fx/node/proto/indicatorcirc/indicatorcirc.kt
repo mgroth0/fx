82596370db37b8a65f2ef7ced1f7716fa3cb2194
mgroth0 <mgroth49@gmail.com>
2023-01-04 15:16:20 -0500

    autocommit

--- node/proto/src/main/kotlin/matt/fx/node/proto/indicatorcirc/indicatorcirc.kt
+++ node/proto/src/main/kotlin/matt/fx/node/proto/indicatorcirc/indicatorcirc.kt
@@ -2,8 +2,8 @@ package matt.fx.node.proto.indicatorcirc
 
 import javafx.scene.paint.Color
 import matt.fx.graphics.wrapper.node.shape.circle.CircleWrapper
-import matt.obs.prop.ValProp
 import matt.obs.bind.binding
+import matt.obs.prop.ValProp
 
 fun indicatorCircle(booleanProperty: ValProp<Boolean>) = CircleWrapper(8.0).apply {
   fillProperty.bind(booleanProperty.binding {
