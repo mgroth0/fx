cfc510552ae8f30cbfe8a768e152aaec4a94ea78
mgroth0 <mgroth49@gmail.com>
2023-01-22 11:42:57 -0500

    autocommit

--- control/src/main/kotlin/matt/fx/control/fxapp/fxapp.kt
+++ control/src/main/kotlin/matt/fx/control/fxapp/fxapp.kt
@@ -5,13 +5,20 @@ import com.sun.javafx.util.Logging
 import javafx.application.Application
 import javafx.application.Preloader
 import javafx.application.Preloader.StateChangeNotification.Type.BEFORE_START
+import javafx.geometry.Pos.CENTER
 import javafx.scene.Scene
+import javafx.scene.control.Label
 import javafx.scene.control.ProgressBar
+import javafx.scene.control.TextArea
 import javafx.scene.layout.BorderPane
+import javafx.scene.layout.VBox
+import javafx.scene.text.Font
 import javafx.stage.Stage
 import matt.fx.control.wrapper.wrapped.WrapperServiceImpl
 import matt.fx.graphics.service.WrapperServiceHub
+import matt.fx.graphics.style.insets
 import matt.log.logger.Logger
+import matt.log.profile.err.BugReport
 import matt.log.reporter.TracksTime
 import matt.model.code.report.Reporter
 import kotlin.concurrent.thread
@@ -76,7 +83,15 @@ class FirstPreloader: Preloader() {
 }
 
 
+ val ERROR_POP_UP_TEXT = """
+  This JavaFX Application encountered an error. Please contact the developer via the bug tracker for this application or email (mgroth49@gmail.com) and include the following information:
+  1. Copy and paste the text below
+  2. Provide a list of steps, to the best of your ability, that lead you to this error. This way we can reproduce the issue on our end and have an easier time getting to the bottom of it.
+  """.trimIndent()
+
 class MinimalFXApp: Application() {
+
+
   //  companion object {
   //	var fxStop: (() -> Unit)? = null
   //  }
@@ -86,7 +101,36 @@ class MinimalFXApp: Application() {
 
 	/* dodge "Unsupported JavaFX configuration..." part 2 */
 	Logging.getJavaFXLogger().enableLogging()
-	fxBlock(parameters.raw)
+	try {
+	  fxBlock(parameters.raw)
+	} catch (e: Exception) {
+	  val debugScene = Scene(
+		VBox(
+		  Label(ERROR_POP_UP_TEXT).apply {
+			isWrapText = true
+			font = Font.font(18.0)
+			insets(25.0)
+		  },
+		  VBox().apply {
+			minHeight = 50.0
+		  },
+		  TextArea(BugReport(t = Thread.currentThread(), e = e).text).apply {
+			insets(25.0)
+		  }
+		).apply {
+		  alignment = CENTER
+		}
+	  )
+	  primaryStage!!.apply {
+		title = "JavaFX Application Failed To Start"
+		scene = debugScene
+		width = 800.0
+		height = 500.0
+		centerOnScreen()
+	  }
+	  primaryStage.show()
+	}
+
 	fxStopwatch?.toc("finished starting main app")
   }
 
