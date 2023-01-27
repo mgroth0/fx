bc1766dc95e98599e096700a192c2154c1759120
mgroth0 <mgroth49@gmail.com>
2023-01-27 15:33:24 -0500

    autocommit

--- node/proto/src/main/kotlin/matt/fx/node/proto/infosymbol/infosymbol.kt
+++ node/proto/src/main/kotlin/matt/fx/node/proto/infosymbol/infosymbol.kt
@@ -1,8 +1,14 @@
 package matt.fx.node.proto.infosymbol
 
+import javafx.scene.paint.Color
+import javafx.scene.text.Font
+import javafx.scene.text.FontPosture.ITALIC
+import javafx.scene.text.FontWeight.BOLD
 import matt.fx.control.wrapper.tooltip.tooltip
+import matt.fx.graphics.font.fixed
 import matt.fx.graphics.wrapper.ET
 import matt.fx.graphics.wrapper.node.attachTo
+import matt.fx.graphics.wrapper.node.shape.circle.circle
 import matt.fx.graphics.wrapper.pane.stack.StackPaneW
 import matt.fx.graphics.wrapper.text.text
 import matt.lang.function.DSL
@@ -11,10 +17,18 @@ fun ET.infoSymbol(text: String, op: DSL<InfoSymbol> = {}) = InfoSymbol(text).att
 
 class InfoSymbol(info: String): StackPaneW() {
   init {
-	/*	circle(radius = 15.0) {
-		  fill = FXColor.GRAY
-		}*/
-	text("\uD83D\uDEC8")
+	circle(radius = 15.0) {
+	  stroke = Color.GRAY
+	  fill = Color.TRANSPARENT
+	}
+	text("i") {
+	  font = Font.font("Georgia").fixed().copy(
+		posture = ITALIC,
+		size = 15.0,
+		weight = BOLD
+	  ).fx()
+	}
+	/*text("\uD83D\uDEC8")*/
 	tooltip(info) {
 	  comfortablyShowForeverUntilMouseMoved()
 	}
