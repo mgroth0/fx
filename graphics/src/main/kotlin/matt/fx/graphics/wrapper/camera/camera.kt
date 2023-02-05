91fc17131881029178e56b23fc2f26d2f222c89d
mgroth0 <mgroth49@gmail.com>
2023-02-05 13:46:23 -0500

    autocommit

--- graphics/src/main/kotlin/matt/fx/graphics/wrapper/camera/camera.kt
+++ graphics/src/main/kotlin/matt/fx/graphics/wrapper/camera/camera.kt
@@ -5,7 +5,6 @@ import javafx.scene.ParallelCamera
 import javafx.scene.PerspectiveCamera
 import matt.fx.graphics.wrapper.node.NodeWrapper
 import matt.fx.graphics.wrapper.node.impl.NodeWrapperImpl
-import matt.fx.graphics.wrapper.node.impl.NodeWrapperImpl
 
 abstract class CameraWrapper(node: Camera): NodeWrapperImpl<Camera>(node) {
   override fun addChild(child: NodeWrapper, index: Int?) {
