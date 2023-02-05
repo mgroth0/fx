a9c661a910a8263d07d531a425eb9c72b0d85aa1
mgroth0 <mgroth49@gmail.com>
2023-02-05 15:54:56 -0500

    autocommit

--- control/src/main/kotlin/matt/fx/control/control.kt
+++ control/src/main/kotlin/matt/fx/control/control.kt
@@ -12,7 +12,6 @@ import javafx.scene.layout.Pane
 import javafx.scene.layout.RowConstraints
 import matt.fx.control.tfx.dialog.alert
 import matt.fx.control.tfx.dialog.asyncAlert
-import matt.fx.control.win.interact.popupTextInput
 import matt.fx.control.wrapper.checkbox.CheckBoxWrapper
 import matt.fx.control.wrapper.control.tab.TabWrapper
 import matt.fx.control.wrapper.scroll.ScrollPaneWrapper
@@ -104,100 +103,4 @@ class TreeTableTreeView<T>(val table: Boolean): TreeTableView<T>() {
 	  header.isVisible = false
 	}
   }
-}
-
-
-object FXActionAbilitiesService: ActionAbilitiesService {
-  override fun confirm(s: String): Boolean {
-	return ensureInFXThreadInPlace {
-	  var ok = false
-	  alert(CONFIRMATION, s) {
-		ok = it == ButtonType.OK
-	  }
-	  ok
-	}
-  }
-
-  override fun yesOrNo(s: String): YesOrNo {
-	return ensureInFXThreadInPlace {
-	  var ok: YesOrNo? = null
-	  alert(
-		CONFIRMATION,
-		s
-	  ) {
-		ok = when (it) {
-		  ButtonType.YES -> YES
-		  ButtonType.NO  -> NO
-		  else           -> NEVER
-		}
-	  }
-	  ok!!
-	}
-  }
-
-  override fun <E: Enum<E>> enum(enumOptions: Array<E>, prompt: String): E {
-	TODO("Not yet implemented")
-  }
-
-  override fun input(prompt: String) = popupTextInput(prompt) ?: NEVER
-}
-
-
-class AsyncFXActionAbilitiesService(
-  private val stage: StageWrapper
-): ActionAbilitiesService {
-  override fun confirm(s: String): Boolean {
-	require(!Platform.isFxApplicationThread())
-	val response = runLaterReturn {
-	  asyncAlert(
-		CONFIRMATION,
-		"Confirmation",
-		s,
-		closeOnEscape = false,
-		owner = stage
-	  ) {
-		runLater {
-		  x = stage.x + (stage.width/2.0) - (width/2.0)
-		  y = stage.y - height
-		}
-	  }
-	}
-
-	return response.join {
-	  it == ButtonType.OK
-	}
-
-  }
-
-  override fun yesOrNo(s: String): YesOrNo {
-	require(!Platform.isFxApplicationThread())
-	val response = ensureInFXThreadInPlace {
-	  asyncAlert(
-		CONFIRMATION,
-		"Yes or No?",
-		s,
-		buttons = arrayOf(ButtonType.NO, ButtonType.YES),
-		closeOnEscape = false,
-		owner = stage,
-	  ) {
-		runLater {
-		  x = stage.x + (stage.width/2.0) - (width/2.0)
-		  y = stage.y - height
-		}
-	  }
-	}
-	return response.join {
-	  when (it) {
-		ButtonType.YES -> YES
-		ButtonType.NO  -> NO
-		else           -> NEVER
-	  }
-	}
-  }
-
-  override fun <E: Enum<E>> enum(enumOptions: Array<E>, prompt: String): E {
-	TODO("Not yet implemented")
-  }
-
-  override fun input(prompt: String) = popupTextInput(prompt) ?: NEVER
 }
