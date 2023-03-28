a4c2090bc9d0ffa23ae0dbd18d7ae9eeb6334640
mgroth0 <mgroth49@gmail.com>
2023-03-28 01:13:37 -0400

    autocommit

--- graphics/src/main/kotlin/matt/fx/graphics/dur/dur.kt
+++ graphics/src/main/kotlin/matt/fx/graphics/dur/dur.kt
@@ -4,5 +4,6 @@ import javafx.util.Duration
 import matt.fx.base.time.toFXDuration
 import matt.time.dur.wrap.DurationWrapper
 
+val YesIUse by lazy { DurationWrapper::class }
 
 fun DurationWrapper.toFXDuration(): Duration = dur.toFXDuration()
