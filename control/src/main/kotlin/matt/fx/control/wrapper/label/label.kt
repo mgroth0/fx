53ca63f14128fde12de1b241a2d3fe7666d21c13
mgroth0 <mgroth49@gmail.com>
2023-04-14 22:11:12 -0400

    changed control/src/main/kotlin/matt/fx/control/wrapper/label/label.kt

--- control/src/main/kotlin/matt/fx/control/wrapper/label/label.kt
+++ control/src/main/kotlin/matt/fx/control/wrapper/label/label.kt
@@ -15,58 +15,58 @@ import matt.obs.prop.ValProp
 
 
 inline fun <reified T> ET.label(
-  observable: ObsVal<T>,
-  graphicProperty: ValProp<out NW?>? = null,
-  converter: StringConverter<T>? = null,
-  /*tooltip: Boolean = false,*/
-  noinline op: LabelWrapper.()->Unit = {}
+    observable: ObsVal<T>,
+    graphicProperty: ValProp<out NW?>? = null,
+    converter: StringConverter<T>? = null,
+    /*tooltip: Boolean = false,*/
+    noinline op: LabelWrapper.() -> Unit = {}
 ) = label().apply {
-  if (converter == null) {
-	if (T::class == String::class) {
-	  @Suppress("UNCHECKED_CAST")
-	  textProperty.bind(observable as ObsVal<String>)
-	} else {
-	  textProperty.bind(observable.binding { it?.toString() })
-	}
-  } else {
-	textProperty.bind(observable.binding { converter.toString(it) })
-  }
-  if (graphicProperty != null) {
-	this.graphicProperty.bind(graphicProperty)
-  }
-/*  if (tooltip) {
-	tooltip {
-	  content = LabelWrapper().also {
-		it.textProperty.bind(this@apply.textProperty)
-	  }
-	}
-  }*/
-  op(this)
+    if (converter == null) {
+        if (T::class == String::class) {
+            @Suppress("UNCHECKED_CAST")
+            textProperty.bind(observable as ObsVal<String>)
+        } else {
+            textProperty.bind(observable.binding { it?.toString() })
+        }
+    } else {
+        textProperty.bind(observable.binding { converter.toString(it) })
+    }
+    if (graphicProperty != null) {
+        this.graphicProperty.bind(graphicProperty)
+    }
+    /*  if (tooltip) {
+        tooltip {
+          content = LabelWrapper().also {
+            it.textProperty.bind(this@apply.textProperty)
+          }
+        }
+      }*/
+    op(this)
 }
 
-fun ET.label(text: String = "", graphic: NodeWrapper? = null, wrap: Boolean? = null, op: LabelWrapper.()->Unit = {}) =
-  LabelWrapper().apply { this.text = text }.attachTo(this, op) {
-	if (graphic != null) it.graphic = graphic
-	if (wrap != null) it.isWrapText = wrap
-  }
+fun ET.label(text: String = "", graphic: NodeWrapper? = null, wrap: Boolean? = null, op: LabelWrapper.() -> Unit = {}) =
+    LabelWrapper().apply { this.text = text }.attachTo(this, op) {
+        if (graphic != null) it.graphic = graphic
+        if (wrap != null) it.isWrapText = wrap
+    }
 
 
 open class LabelWrapper(
-  node: Label = Label(),
-): LabeledWrapper<Label>(node) {
+    node: Label = Label(),
+) : LabeledWrapper<Label>(node) {
 
-  constructor(
-	text: String?,
-	graphic: Node? = null,
-	wrap: Boolean? = null
-  ): this(Label(text, graphic).apply {
-	if (wrap != null) isWrapText = wrap
-  })
+    constructor(
+        text: String?,
+        graphic: Node? = null,
+        wrap: Boolean? = null
+    ) : this(Label(text, graphic)) {
+        if (wrap != null) isWrapText = wrap
+    }
 
-  override fun addChild(child: NodeWrapper, index: Int?) {
-	require(index == null)
-	graphic = child
-  }
+    override fun addChild(child: NodeWrapper, index: Int?) {
+        require(index == null)
+        graphic = child
+    }
 
 
 }
