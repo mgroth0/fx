5ee45ba66e435cfe52eb0d5f7c7532cf494bb091
mgroth0 <mgroth49@gmail.com>
2023-04-18 17:37:07 -0400

    changed control/src/main/kotlin/matt/fx/control/fxapp/fxapp.kt

--- control/src/main/kotlin/matt/fx/control/fxapp/fxapp.kt
+++ control/src/main/kotlin/matt/fx/control/fxapp/fxapp.kt
@@ -25,61 +25,61 @@ import kotlin.concurrent.thread
 
 
 fun runFXAppBlocking(
-  args: Array<String>,
-  usePreloaderApp: Boolean = false,
-  reporter: Reporter? = null,
-  fxOp: (List<String>)->Unit,
+    args: Array<String>,
+    usePreloaderApp: Boolean = false,
+    reporter: Reporter? = null,
+    fxOp: (List<String>) -> Unit,
 ) {
-  WrapperServiceHub.install(WrapperServiceImpl)
-  (reporter as? TracksTime)?.toc("running FX App")
-  fxBlock = fxOp
-  thread(isDaemon = true) {
+    WrapperServiceHub.install(WrapperServiceImpl)
+    (reporter as? TracksTime)?.toc("running FX App")
+    fxBlock = fxOp
+    thread(isDaemon = true) {
 
-	Logging.getJavaFXLogger().disableLogging() /* dodge "Unsupported JavaFX configuration..." part 1 */
-  }
-  (reporter as? TracksTime)?.toc("started disabling FX logging")
-  fxStopwatch = (reporter as? TracksTime)
-  if (usePreloaderApp) {
-	(reporter as? TracksTime)?.toc("launching preloader")
-	LauncherImpl.launchApplication(MinimalFXApp::class.java, FirstPreloader::class.java, args)
-  } else {
-	(reporter as? TracksTime)?.toc("launching app")
-	Application.launch(MinimalFXApp::class.java, *args)
-  }
-  (reporter as? Logger)?.info("main thread has exited from Application.launch")
+        Logging.getJavaFXLogger().disableLogging() /* dodge "Unsupported JavaFX configuration..." part 1 */
+    }
+    (reporter as? TracksTime)?.toc("started disabling FX logging")
+    fxStopwatch = (reporter as? TracksTime)
+    if (usePreloaderApp) {
+        (reporter as? TracksTime)?.toc("launching preloader")
+        LauncherImpl.launchApplication(MinimalFXApp::class.java, FirstPreloader::class.java, args)
+    } else {
+        (reporter as? TracksTime)?.toc("launching app")
+        Application.launch(MinimalFXApp::class.java, *args)
+    }
+    (reporter as? Logger)?.info("main thread has exited from Application.launch")
 }
 
 private var fxStopwatch: TracksTime? = null
-private lateinit var fxBlock: (List<String>)->Unit
+private lateinit var fxBlock: (List<String>) -> Unit
 
 
-class FirstPreloader: Preloader() {
-  private var bar: ProgressBar? = null
-  var stage: Stage? = null
-  private fun createPreloaderScene(): Scene {
-	bar = ProgressBar()
-	val p = BorderPane()
-	p.center = bar
-	return Scene(p, 300.0, 150.0)
-  }
+class FirstPreloader : Preloader() {
+    private var bar: ProgressBar? = null
+    var stage: Stage? = null
+    private fun createPreloaderScene(): Scene {
+        bar = ProgressBar()
+        val p = BorderPane()
+        p.center = bar
+        return Scene(p, 300.0, 150.0)
+    }
 
-  override fun start(stage: Stage) {
-	fxStopwatch?.toc("starting preloader app")
-	this.stage = stage
-	stage.scene = createPreloaderScene()
-	stage.show()
-	fxStopwatch?.toc("finished starting preloader app")
-  }
+    override fun start(stage: Stage) {
+        fxStopwatch?.toc("starting preloader app")
+        this.stage = stage
+        stage.scene = createPreloaderScene()
+        stage.show()
+        fxStopwatch?.toc("finished starting preloader app")
+    }
 
-  override fun handleProgressNotification(pn: ProgressNotification) {
-	bar!!.progress = pn.progress
-  }
+    override fun handleProgressNotification(pn: ProgressNotification) {
+        bar!!.progress = pn.progress
+    }
 
-  override fun handleStateChangeNotification(evt: StateChangeNotification) {
-	if (evt.type == BEFORE_START) {
-	  stage!!.hide()
-	}
-  }
+    override fun handleStateChangeNotification(evt: StateChangeNotification) {
+        if (evt.type == BEFORE_START) {
+            stage!!.hide()
+        }
+    }
 }
 
 
@@ -87,56 +87,59 @@ val ERROR_POP_UP_TEXT = """
   The application has encountered an unexpected error. Please submit a bug report. To help further, leave a comment in the bug report listing the steps you took that caused the error.
   """.trim()
 
-class MinimalFXApp: Application() {
+class MinimalFXApp : Application() {
 
 
-  //  companion object {
-  //	var fxStop: (() -> Unit)? = null
-  //  }
-  override fun start(primaryStage: Stage?) {
+    //  companion object {
+    //	var fxStop: (() -> Unit)? = null
+    //  }
+    override fun start(primaryStage: Stage?) {
 
-	fxStopwatch?.toc("starting main app")
+        fxStopwatch?.toc("starting main app")
 
-	/* dodge "Unsupported JavaFX configuration..." part 2 */
-	Logging.getJavaFXLogger().enableLogging()
-	try {
-	  fxBlock(parameters.raw)
-	} catch (e: Exception) {
-	  val bugText = BugReport(t = Thread.currentThread(), e = e).text
-	  println("\n\n$bugText\n\n")
-	  val debugScene = Scene(
-		VBox(
-		  Label(ERROR_POP_UP_TEXT).apply {
-			isWrapText = true
-			font = Font.font(18.0)
-			insets(25.0)
-		  },
-		  VBox().apply {
-			minHeight = 50.0
-		  },
-		  TextArea(bugText).apply {
-			insets(25.0)
-		  }
-		).apply {
-		  alignment = CENTER
-		}
-	  )
-	  primaryStage!!.apply {
-		title = "JavaFX Application Failed To Start"
-		scene = debugScene
-		width = 800.0
-		height = 500.0
-		centerOnScreen()
-	  }
-	  primaryStage.show()
+        /* dodge "Unsupported JavaFX configuration..." part 2 */
+        Logging.getJavaFXLogger().enableLogging()
+        try {
+            fxBlock(parameters.raw)
+        } catch (e: Exception) {
+            val bugText = BugReport(t = Thread.currentThread(), e = e).text
+            println("\n\n$bugText\n\n")
+            val debugScene = Scene(
+                VBox(
+                    Label(ERROR_POP_UP_TEXT).apply {
+                        isWrapText = true
+                        font = Font.font(18.0)
+                        insets(25.0)
+                    },
+                    VBox().apply {
+                        minHeight = 50.0
+                    },
+                    TextArea(bugText).apply {
+                        insets(25.0)
+                    }
+                ).apply {
+                    alignment = CENTER
+                }
+            )
+            primaryStage!!.apply {
+                title = "JavaFX Application Failed To Start"
+                scene = debugScene
+                width = 800.0
+                height = 500.0
+                centerOnScreen()
+            }
+            primaryStage.show()
 
-	}
+        }
 
-	fxStopwatch?.toc("finished starting main app")
-  }
+        fxStopwatch?.toc("finished starting main app")
+    }
 
-  /*DO_NOT_SHUTDOWN_WITH_FX_THREAD*//*DO_NOT_SHUTDOWN_WITH_FX_THREAD*//*DO_NOT_SHUTDOWN_WITH_FX_THREAD*//*DO_NOT_SHUTDOWN_WITH_FX_THREAD*//*DO_NOT_SHUTDOWN_WITH_FX_THREAD*//*DO_NOT_SHUTDOWN_WITH_FX_THREAD*/
-  override fun stop() {    /*DO_NOT_SHUTDOWN_WITH_FX_THREAD*/    /*DO_NOT_SHUTDOWN_WITH_FX_THREAD*/    /*DO_NOT_SHUTDOWN_WITH_FX_THREAD*/    /*DO_NOT_SHUTDOWN_WITH_FX_THREAD*/    /*DO_NOT_SHUTDOWN_WITH_FX_THREAD*/    /*DO_NOT_SHUTDOWN_WITH_FX_THREAD*/
-  }/*DO_NOT_SHUTDOWN_WITH_FX_THREAD*//*DO_NOT_SHUTDOWN_WITH_FX_THREAD*//*DO_NOT_SHUTDOWN_WITH_FX_THREAD*//*DO_NOT_SHUTDOWN_WITH_FX_THREAD*//*DO_NOT_SHUTDOWN_WITH_FX_THREAD*//*DO_NOT_SHUTDOWN_WITH_FX_THREAD*/
+    /*DO_NOT_SHUTDOWN_WITH_FX_THREAD*//*DO_NOT_SHUTDOWN_WITH_FX_THREAD*//*DO_NOT_SHUTDOWN_WITH_FX_THREAD*//*DO_NOT_SHUTDOWN_WITH_FX_THREAD*//*DO_NOT_SHUTDOWN_WITH_FX_THREAD*//*DO_NOT_SHUTDOWN_WITH_FX_THREAD*/
+    override fun stop() {
+
+        println("DEBUG: STOP FX APP")
+        /*DO_NOT_SHUTDOWN_WITH_FX_THREAD*/    /*DO_NOT_SHUTDOWN_WITH_FX_THREAD*/    /*DO_NOT_SHUTDOWN_WITH_FX_THREAD*/    /*DO_NOT_SHUTDOWN_WITH_FX_THREAD*/    /*DO_NOT_SHUTDOWN_WITH_FX_THREAD*/    /*DO_NOT_SHUTDOWN_WITH_FX_THREAD*/
+    }/*DO_NOT_SHUTDOWN_WITH_FX_THREAD*//*DO_NOT_SHUTDOWN_WITH_FX_THREAD*//*DO_NOT_SHUTDOWN_WITH_FX_THREAD*//*DO_NOT_SHUTDOWN_WITH_FX_THREAD*//*DO_NOT_SHUTDOWN_WITH_FX_THREAD*//*DO_NOT_SHUTDOWN_WITH_FX_THREAD*/
 }
 
