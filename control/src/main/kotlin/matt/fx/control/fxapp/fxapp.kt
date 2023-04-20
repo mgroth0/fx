bf8c9fe6aed41d9846fac2642cf7024cb3c2ac35
mgroth0 <mgroth49@gmail.com>
2023-04-20 11:22:15 -0400

    changed control/src/main/kotlin/matt/fx/control/fxapp/fxapp.kt

--- control/src/main/kotlin/matt/fx/control/fxapp/fxapp.kt
+++ control/src/main/kotlin/matt/fx/control/fxapp/fxapp.kt
@@ -3,8 +3,10 @@ package matt.fx.control.fxapp
 import com.sun.javafx.application.LauncherImpl
 import com.sun.javafx.util.Logging
 import javafx.application.Application
+import javafx.application.Platform
 import javafx.application.Preloader
 import javafx.application.Preloader.StateChangeNotification.Type.BEFORE_START
+import javafx.event.Event
 import javafx.geometry.Pos.CENTER
 import javafx.scene.Scene
 import javafx.scene.control.Label
@@ -14,9 +16,15 @@ import javafx.scene.layout.BorderPane
 import javafx.scene.layout.VBox
 import javafx.scene.text.Font
 import javafx.stage.Stage
+import javafx.stage.Window
+import javafx.stage.WindowEvent
+import matt.collect.itr.recurse.chain
 import matt.fx.control.wrapper.wrapped.WrapperServiceImpl
 import matt.fx.graphics.service.WrapperServiceHub
 import matt.fx.graphics.style.insets
+import matt.lang.anno.Fixes
+import matt.lang.function.Op
+import matt.lang.go
 import matt.log.logger.Logger
 import matt.log.report.BugReport
 import matt.log.reporter.TracksTime
@@ -64,6 +72,8 @@ class FirstPreloader : Preloader() {
     }
 
     override fun start(stage: Stage) {
+
+
         fxStopwatch?.toc("starting preloader app")
         this.stage = stage
         stage.scene = createPreloaderScene()
@@ -87,6 +97,130 @@ val ERROR_POP_UP_TEXT = """
   The application has encountered an unexpected error. Please submit a bug report. To help further, leave a comment in the bug report listing the steps you took that caused the error.
   """.trim()
 
+
+/*This is taken straight from JavaFX Source Code*/
+@Fixes("Big NullPointerException Bug")
+private class DefaultGlassAppEventHandler(
+    private val buildInHandler: com.sun.glass.ui.Application.EventHandler
+) : com.sun.glass.ui.Application.EventHandler() {
+    override fun handleQuitAction(app: com.sun.glass.ui.Application?, time: Long) {
+
+        /*Emulate:
+
+              com.sun.javafx.tk.quantum.GlassStage.requestClosingAllWindows
+
+              with the NullPointerException Fix
+
+              */
+
+        /*if (Platform.isNestedLoopRunning()) {
+            Toolkit.getToolkit().exitAllNestedEventLoops()
+        }*/
+
+
+
+
+        @Fixes("Big NullPointerException Bug")
+        tryToEscapeNestedEventLoopsAndThenFinally {
+            @Fixes("Big NullPointerException Bug")
+            buildInHandler.handleQuitAction(app, time)
+        }
+
+
+    }
+
+    override fun handleThemeChanged(themeName: String?): Boolean {
+        return buildInHandler.handleThemeChanged(themeName)
+        /*val highContrastSchemeName: String =
+            com.sun.glass.ui.Application.GetApplication().getHighContrastScheme(themeName)
+        return PlatformImpl.setAccessibilityTheme(highContrastSchemeName)*/
+    }
+
+
+    /*After thinking a lot about this, I think the only sane solution is to first ensure tht all currently running events are completed if in a nestedLoop. This seems like the only way to gracefully exit without a NullPointerException.*/
+    @Fixes("Big NullPointerException Bug")
+    private fun tryToEscapeNestedEventLoopsAndThenFinally(depth: Int = 0, op: Op) {
+        if (depth < 10 && Platform.isNestedLoopRunning()) {
+            tryToCloseDeepestDialog()
+            Platform.runLater {
+                tryToEscapeNestedEventLoopsAndThenFinally(depth = depth + 1, op)
+            }
+        } else op()
+    }
+
+}
+//
+//@Fixes("Big NullPointerException Bug")
+//private class MyGlassApEventHandler : DefaultGlassAppEventHandler() {
+//    override fun handleQuitAction(app: com.sun.glass.ui.Application?, time: Long) {
+//
+//
+//    }
+//
+//}
+
+@Fixes("Big NullPointerException Bug")
+private fun tryToCloseDeepestDialog() {
+
+    Window.getWindows().filterIsInstance<Stage>().filter {
+        Stage::class.java.getDeclaredField("inNestedEventLoop").run {
+            isAccessible = true
+            get(it) as Boolean
+        }
+    }.sortedByDescending {
+        (it as Window).chain { (it as? Stage)?.owner }.count()
+    }.firstOrNull()?.go {
+        Event.fireEvent(
+            it,
+            WindowEvent(
+                it,
+                WindowEvent.WINDOW_CLOSE_REQUEST
+            )
+        )
+    }
+}
+
+/*
+@Fixes("Big NullPointerException Bug")
+private fun quitJavaFX() {
+
+
+    Window.getWindows().asSequence().filterIsInstance<Stage>()
+        .firstOrNull { it.isFullScreen }?.isFullScreen = false
+
+    Window.getWindows().toList().sortedByDescending {
+        *//*ATTEMPT TO FIX THE BIG NullPointerException Bug Part 1*//*
+        var score = 0
+
+        if (it is Stage && it.modality == Modality.APPLICATION_MODAL) score++
+        if (it is PopupWindow) score++
+        if (it is Stage && Stage::class.java.getDeclaredField("inNestedEventLoop").run {
+                isAccessible = true
+                get(it) as Boolean
+            }) {
+            score = 2
+        } else if (it is Stage && it.modality == Modality.APPLICATION_MODAL) {
+            score++
+        } else if (it is PopupWindow) score++
+
+        score
+
+    }.forEach {
+        *//*In case of child windows some of them could already be closed
+        so check if list still contains an object*//*
+        if (it in Window.getWindows() && it.isShowing) {
+            *//*it.hide()*//*
+            Event.fireEvent(
+                it,
+                WindowEvent(
+                    it,
+                    WindowEvent.WINDOW_CLOSE_REQUEST
+                )
+            )
+        }
+    }
+}*/
+
 class MinimalFXApp : Application() {
 
 
@@ -94,6 +228,9 @@ class MinimalFXApp : Application() {
     //	var fxStop: (() -> Unit)? = null
     //  }
     override fun start(primaryStage: Stage?) {
+
+        /*GOAL: TO PREVENT THE BIG NullPointerException BUG THAT I JUST EMAILED THE OPENJFX LISTSERV ABOUT*/
+        com.sun.glass.ui.Application.GetApplication().eventHandler = DefaultGlassAppEventHandler(com.sun.glass.ui.Application.GetApplication().eventHandler)
 
         fxStopwatch?.toc("starting main app")
 
