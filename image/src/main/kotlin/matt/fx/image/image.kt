799f3bbd7ff05e5e51f760bb82f009f219fcc316
mgroth0 <mgroth49@gmail.com>
2023-06-09 11:42:22 -0400

    changed image/src/main/kotlin/matt/fx/image/image.kt

--- image/src/main/kotlin/matt/fx/image/image.kt
+++ image/src/main/kotlin/matt/fx/image/image.kt
@@ -1,5 +1,6 @@
 package matt.fx.image
 
+import javafx.application.Platform
 import javafx.embed.swing.SwingFXUtils
 import javafx.scene.image.Image
 import matt.file.MFile
@@ -9,18 +10,32 @@ import javax.imageio.ImageIO
 
 
 fun jswingIconToImage(jswingIcon: javax.swing.Icon): Image? {
-  val bufferedImage = BufferedImage(
-	jswingIcon.iconWidth, jswingIcon.iconHeight,
-	BufferedImage.TYPE_INT_ARGB
-  )
-  jswingIcon.paintIcon(null, bufferedImage.graphics, 0, 0)
-  return SwingFXUtils.toFXImage(bufferedImage, null)
+    require(Platform.isFxApplicationThread()) {
+        "Let's try to be extra safe and only do this one on the FX Application thread too"
+    }
+    val bufferedImage = BufferedImage(
+        jswingIcon.iconWidth, jswingIcon.iconHeight,
+        BufferedImage.TYPE_INT_ARGB
+    )
+    jswingIcon.paintIcon(null, bufferedImage.graphics, 0, 0)
+    return SwingFXUtils.toFXImage(bufferedImage, null)
 }
 
-fun Image.toBufferedImage(): BufferedImage = SwingFXUtils.fromFXImage(this, null)
-fun BufferedImage.toFXImage(): Image = SwingFXUtils.toFXImage(this, null)
+fun Image.toBufferedImage(): BufferedImage {
+    require(Platform.isFxApplicationThread()) {
+        "Let's try to be extra safe and only do this one on the FX Application thread too"
+    }
+    return SwingFXUtils.fromFXImage(this, null)
+}
+
+fun BufferedImage.toFXImage(): Image {
+    require(Platform.isFxApplicationThread()) {
+        "I think there is a somewhat hard to reproduce but horrible deadlock that could happen if this is not done on the JavaFX application thread. But that should be fine, since this method only needs to happen in FX applications anyway. By the way, the place I saw this deadlock was when trying to do a large find operation in brainstorm, which caused lots of favicons and icons to be asynchronously loaded"
+    }
+    return SwingFXUtils.toFXImage(this, null)
+}
 
 fun Image.save(file: MFile): MFile {
-  ImageIO.write(toBufferedImage(), file.extension, file)
-  return file
+    ImageIO.write(toBufferedImage(), file.extension, file)
+    return file
 }
